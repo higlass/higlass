d5611715d3f370b51ce3e37a851f08ff8ddc142c
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2016-01-19 19:04:07 +0100

    Adding gene info

--- scripts/genes_by_popularity.py
+++ scripts/genes_by_popularity.py
@@ -23,6 +23,18 @@ gene2refseq = (sc.textFile(op.join(base_dir, "data/gene2refseq"))
                                     'nucleotide_accession': p[7],
                                     'orientation': p[11],
                                     'assembly': p[12]}))
+                                    
+gene_info = (sc.textFile(op.join(base_dir, "data/gene_info"))
+             .filter(lambda x: x[0] != '#')
+             .map(lambda x: x.split('\t'))
+             .map(lambda x: { 'taxid': x[0],
+                              'geneid': x[1],
+                              'description': x[8],
+                              'symbol': x[2],
+                              'name': x[11]}))
+                              
+gene_info_keyed = gene_info.map(lambda x: ((x['taxid'], x['geneid']), x))
+                              
 #schemaGene2Refseq = sqlContext.inferSchema(gene2refseq)
 #schemaGene2Refseq.registerTempTable("gene2refseq")
 
@@ -54,19 +66,27 @@ def reduce_count(r1, r2):
 counted_human_gene_pubmed = (human_gene_pubmed.reduceByKey(reduce_count))
 counted_human_gene_pubmed.take(1)
 
+def merge_two_dicts(x, y):
+    '''Given two dicts, merge them into a new dict as a shallow copy.'''
+    z = x.copy()
+    z.update(y)
+    return z
+
 # filter the refseq genes to those in the human GRCh38 assembly
 # ((taxid, geneid), row)
 human_refseq = (gene2refseq.filter(lambda x: x['assembly'].find('GRCh38') >= 0)
                            .filter(lambda x: x['nucleotide_accession'].find('NC_') >= 0)
                            .map(lambda x: ((x['taxid'], x['geneid']), x)))
 
+human_refseq_info = (human_refseq.join(gene_info_keyed)
+                    .map(lambda x: (x[0], merge_two_dicts(x[1][0], x[1][1]))))
+
 # join (K,V) and (K,W) -> (K, (V,W)) pairs
 # map (K,(V,W)) -> (K,W)
 # join the genes with reference counts with the refseq information
 human_refseq_pubmed = (counted_human_gene_pubmed.join(human_refseq)
-                      .map(lambda x: ((x[1][0]['count'], x[0][0], x[0][1]), x[1][1]))
-                      .map(lambda x: x['start_end_pos'] = (x['start_pos'], x['end_pos']))
-                      
+                      .map(lambda x: ((x[1][0]['count'], x[0][0], x[0][1]), x[1][1])))
+                      #.map(lambda x: x['start_end_pos'] = (x['nucleotide_accession'], x['orientation'], x['start_pos'], x['end_pos']))
                       
 def consolidate_start_and_end(r):
     '''
@@ -76,7 +96,7 @@ def consolidate_start_and_end(r):
     :param r: (key, {'start_pos': 1000, 'end_pos': 1010})
     :return: (key, {'start_end_pos': set((1000, 1010))}
     '''
-    r[1]['start_end_pos'] = set([(int(r[1]['start_pos']), int(r[1]['end_pos']))])
+    r[1]['start_end_pos'] = set([(r[1]['nucleotide_accession'], r[1]['orientation'],int(r[1]['start_pos']), int(r[1]['end_pos']))])
     return (r[0], r[1])
 
 def reduce_by_start_end_pos(r1,r2):
@@ -85,7 +105,7 @@ def reduce_by_start_end_pos(r1,r2):
     
     :param r: {'start_end_pos': set((1000, 1010))}
     '''
-    print >>sys.stderr, "r1:", r1
+    #print >>sys.stderr, "r1:", r1
     r1['start_end_pos'] = r1['start_end_pos'].union(r2['start_end_pos'])
     return r1
     
@@ -94,9 +114,15 @@ reduced_human_refseq_pubmed = (human_refseq_pubmed.map(consolidate_start_and_end
                                .reduceByKey(reduce_by_start_end_pos))
 
               
-reduced_human_refseq_pubmed.sortByKey(ascending=False).take(10)
+reduced_human_refseq_pubmed.sortByKey(ascending=False)
+reduced_human_refseq_pubmed.take(1)
 
-counted_human_refseq_pubmed.take(10)
+# take every (chr, orientation, start, end) tuple from the set and create one
+# big list out of it
+# then convert it all to TSV strings
+flattened_human_refseq_pubmed = (reduced_human_refseq_pubmed.flatMap(lambda x: [[x[0][0]] + list(y) for y in x[1]['start_end_pos']])
+                                 .map(lambda x: "\t".join(map(str, x)))) 
+flattened_human_refseq_pubmed.saveAsTextFile('/scr/fluidspace/pkerp/projects/goomba/output/genes_by_popularity')
 
 '''
 gene_pubmed = sqlContext.sql("select geneid, start_pos, count(*) as cnt from gene_starts group by geneid, start_pos order by cnt desc")
