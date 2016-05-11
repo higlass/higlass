1b4cb8e0ba67486c1fb99984ce64438be8053302
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-05-11 17:00:11 -0400

    Moved hi-pixi to the root directory

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/app',
+  entry: {higlass: ['./scripts/higlass.js']},
+  output: {
+    path: __dirname + '/build',
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: '[name]'
+  },
+  module: {
+    loaders: [
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }, {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
+  }
+};
