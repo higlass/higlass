1af31ce4b73c1f7d900e2fa73c313d041d81b08b
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-10-20 07:57:48 -0400

    Added chromosome grid

--- app/scripts/MultiTrackContainer.jsx
+++ app/scripts/MultiTrackContainer.jsx
@@ -15,6 +15,7 @@ import {TopDiagonalHeatmapRectangleTrack} from './TopDiagonalHeatmapTrack.js'
 import {AddTrackDiv} from './AddTrackDiv.js'
 import {TopGeneLabelsTrack} from './TopGeneLabelsTrack.js'
 import {LeftGeneLabelsTrack} from './LeftGeneLabelsTrack.js'
+import {ChromosomeGrid} from './ChromosomeGrid.js';
 import {TopChromosomeAxis} from './TopChromosomeAxis.js'
 import {LeftChromosomeAxis} from './LeftChromosomeAxis.js'
 import {GenomePositionSearchBox} from './GenomePositionSearchBox.jsx'
@@ -45,7 +46,8 @@ export class MultiTrackContainer extends React.Component {
                 this.trackDescriptions[tracks[i].type].position == 'center')
                 this.twoD = true
 
-            if (this.trackDescriptions[tracks[i].type].position == 'center')
+            if (this.trackDescriptions[tracks[i].type].position == 'center' && 
+                this.trackDescriptions[tracks[i].type].overlay == false)
                 if (!('height' in tracks[i]))
                     this.heightSpecified = false;
 
@@ -178,6 +180,8 @@ export class MultiTrackContainer extends React.Component {
         if (typeof this.topChromosomeAxis != 'undefined') {
             this.topChromosomeAxis.xScale(this.xOrigScale.copy());
             this.leftChromosomeAxis.yScale(this.yOrigScale.copy());
+            this.chromosomeGrid.xScale(this.xOrigScale.copy());
+            this.chromosomeGrid.yScale(this.yOrigScale.copy());
             this.leftGeneLabels.yScale(this.yOrigScale.copy());
             this.topGeneLabels.xScale(this.xOrigScale.copy());
             this.horizontalTiledArea.xScale(this.xOrigScale.copy());
@@ -236,6 +240,9 @@ export class MultiTrackContainer extends React.Component {
             let trackId = this.state.tracksList[i].uid;
             let track = this.state.tracks[trackId];
 
+            if (track.overlay)
+                continue;       //overlay tracks don't have their dimensions counted
+
             if (this.trackDescriptions[track.type].position == 'top')
                 this.topMargin += track.height;
             if (this.trackDescriptions[track.type].position == 'left')
@@ -254,6 +261,18 @@ export class MultiTrackContainer extends React.Component {
             let track = this.state.tracks[trackId];
             track.leftMargin = this.leftMargin;
             track.topMargin = this.topMargin;
+
+            if (track.overlay) {
+                if (i == 0) {
+                    console.log("The first track can't be an overlay track");
+                    continue;
+                } else {
+                    track.left = this.state.tracksList[i-1].left;
+                    track.right = this.state.tracksList[i-1].right;
+                    track.width = this.state.tracksList[i-1].width;
+                    track.height = this.state.tracksList[i-1].height;
+                }
+            }
 
             if (this.trackDescriptions[track.type].position == 'top') {
                 track.left = this.leftMargin;
@@ -346,7 +365,8 @@ export class MultiTrackContainer extends React.Component {
 
     initLayouts() {
         this.topChromosomeAxis = TopChromosomeAxis();
-        this.leftChromosomeAxis = LeftChromosomeAxis()
+        this.leftChromosomeAxis = LeftChromosomeAxis();
+        this.chromosomeGrid = ChromosomeGrid();
         this.horizontalDiagonalTiledArea = GenericTiledArea()
         this.horizontalTiledArea = GenericTiledArea()
         this.verticalTiledArea = GenericTiledArea()
@@ -400,6 +420,14 @@ export class MultiTrackContainer extends React.Component {
             .zoomDispatch(this.zoomDispatch)
 
         this.leftChromosomeAxis
+            .yScale(this.yOrigScale.copy())
+            .width(this.width)
+            .height(this.height)
+            .resizeDispatch(this.resizeDispatch)
+            .zoomDispatch(this.zoomDispatch)
+
+        this.chromosomeGrid
+            .xScale(this.xOrigScale.copy())
             .yScale(this.yOrigScale.copy())
             .width(this.width)
             .height(this.height)
@@ -635,6 +663,15 @@ export class MultiTrackContainer extends React.Component {
                 tracksPerDimensions[trackDimensions] = [];
 
             tracksPerDimensions[trackDimensions].push(this.state.tracks[trackId]);
+            console.log('track-dimensions', trackDimensions);
+
+            /*
+            for (let i = 0; i < this.state.tracks[trackId].overlays.length; i++) {
+                console.log('adding overlay:', trackDimensions);
+                // add overlays, they should have the same dimensions
+                tracksPerDimensions[trackDimensions].push(this.state.tracks[trackId].overlays[i]);
+            }
+            */
         }
 
         for (let trackDimensions in tracksPerDimensions) {
@@ -651,6 +688,7 @@ export class MultiTrackContainer extends React.Component {
                 
             d3.select(this.bigDiv).selectAll('.' + trackDimensions)
               .data(tracksPerDimensions[trackDimensions])
+              .each(function(d) { console.log('calling... ', d); })
               .call(this.trackDimensionsHandlers[trackDimensions])
         }
 
@@ -752,12 +790,19 @@ export class MultiTrackContainer extends React.Component {
                     'position': 'center',
                     'layout': this.heatmapRectangleTrack,
                     'dimension': 'two-d'
-                } 
+                },
+            'chromosome-grid':
+                {
+                    'position': 'center',
+                    'layout': this.chromsomeGrid,
+                    'dimension': 'chromosome-grid'
+                }
             };
 
         this.trackDimensionsHandlers = {
             'horizontal-axis': this.topChromosomeAxis,
             'vertical-axis': this.leftChromosomeAxis,
+            'chromosome-grid': this.chromosomeGrid,
             'one-d-horizontal-diagonal': this.horizontalDiagonalTiledArea,
             'one-d-horizontal': this.horizontalTiledArea,
             'one-d-vertical': this.verticalTiledArea,
@@ -794,8 +839,8 @@ export class MultiTrackContainer extends React.Component {
     trackOpacity(track) {
         if (track.type == 'top-chromosome-axis' || track.type == 'left-chromosome-axis')
             return 1.;
-        else
-            return 0.1;
+        else if (track.type == 'chromosome-grid') return 0.5
+        else return 0.1;
     }
 
     zoomTo(scale, range) {
