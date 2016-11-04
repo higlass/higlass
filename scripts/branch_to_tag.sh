6c219d4d5d37c0d1638e2844dae6cbe23fae4b6e
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-11-04 16:15:08 -0400

    Added the branch_to_tag script

---
+++ scripts/branch_to_tag.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+BRANCHNAME=$1
+TAGNAME=$2
+
+echo "Request to convert the branch ${BRANCHNAME} to a tag with the same name accepted."
+echo "Processing..."
+echo " "
+
+git show-ref --verify --quiet refs/heads/${BRANCHNAME}
+# $? == 0 means local branch with <branch-name> exists. 
+
+if [ $? == 0 ]; then
+   git checkout ${BRANCHNAME}
+   git tag ${BRANCHNAME}
+   git checkout master
+   git branch ${BRANCHNAME} -d
+   echo " "
+   echo "Updated list branches, sorted chronologically: "
+   echo "---------------------------------------------- "
+   git log --no-walk --date-order --oneline --decorate $(git rev-list --branches --no-walk) | cut -d "(" -f 2 | cut -d ")" -f 1
+else
+   echo "Sorry. The branch ${BRANCHNAME} does NOT seem to exist. Exiting."
+fi
