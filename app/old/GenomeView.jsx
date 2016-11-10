beb5edb684507886c87ef66af7f23ff31414db4d
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-11-10 12:37:44 -0500

    Moved javascript files to new directory in anticipation of a large refactoring event

---
+++ app/old/GenomeView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class SearchableBigPlot extends React.Component {
+    constructor(props) {
+         
+    }
+
+    render() {
+
+    }
+}
