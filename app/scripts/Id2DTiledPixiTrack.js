377615811ea04d241846f290bf0e9373f0e66ac2
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-12-04 14:50:43 -0500

    Center tile id text

--- app/scripts/Id2DTiledPixiTrack.js
+++ app/scripts/Id2DTiledPixiTrack.js
@@ -22,11 +22,25 @@ export class Id2DTiledPixiTrack extends Tiled2DPixiTrack {
          */
          
         tile.textGraphics = new PIXI.Graphics();
-        tile.text = new PIXI.Text(tile.tileData.zoomLevel + "/" + tile.tileData.tilePos.join('/') + '/' + tile.mirrored, 
-                              {fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+        //tile.text = new PIXI.Text(tile.tileData.zoomLevel + "/" + tile.tileData.tilePos.join('/') + '/' + tile.mirrored, 
 
-        tile.text.y = 100;
+        if (tile.mirrored) {
+            // mirrored tiles have their x and y coordinates reversed
+            tile.text = new PIXI.Text(tile.tileData.zoomLevel + "/" + [tile.tileData.tilePos[1], tile.tileData.tilePos[0]].join('/'), 
+                                  {fontFamily : 'Arial', fontSize: 48, fill : 0xff1010, align : 'center'});
+        } else {
+            tile.text = new PIXI.Text(tile.tileData.zoomLevel + "/" + tile.tileData.tilePos.join('/'), 
+                                  {fontFamily : 'Arial', fontSize: 48, fill : 0xff1010, align : 'center'});
+
+        }
+
+        //tile.text.y = 100;
         tile.textGraphics.addChild(tile.text);
+
+        tile.text.anchor.x = 0.5;
+        tile.text.anchor.y = 0.5;
+        
+    
         graphics.addChild(tile.textGraphics);
 
         this.drawTile(tile, graphics);
@@ -40,17 +54,10 @@ export class Id2DTiledPixiTrack extends Tiled2DPixiTrack {
         let {tileX, tileY, tileWidth, tileHeight} = this.getTilePosAndDimensions(tile.tileData.zoomLevel, 
                                                                                  tile.tileData.tilePos);
 
-        console.log('tile', tile);
-        /*
-        t.position.x = 0;
-        t.position.y = 0;
-        */
-
         // the text needs to be scaled down so that it doesn't become huge
         // when we zoom in 
         let tSX = 1 / ((this._xScale(1) - this._xScale(0)) / (this._refXScale(1) - this._refXScale(0)));
         let tSY = 1 / ((this._yScale(1) - this._yScale(0)) / (this._refYScale(1) - this._refYScale(0)));
-
 
         tile.text.scale.x = tSX;
         tile.text.scale.y = tSY;
@@ -58,8 +65,6 @@ export class Id2DTiledPixiTrack extends Tiled2DPixiTrack {
         //console.log('tSX:', tSX, 'tSY:',tSY);
 
         graphics.clear();
-
-        //console.log('4 * tSX:', 4 * tSX);
 
         graphics.lineStyle(4 * tSX, 0x0000FF, 1);
         graphics.beginFill(0xFF700B, 0.4);
@@ -71,19 +76,23 @@ export class Id2DTiledPixiTrack extends Tiled2DPixiTrack {
         // fun tile positioning when it's mirrored, except this is just a rectangle
         // that doesn't need to be rotated so it's easy
         if (tile.mirrored) {
-            tile.textGraphics.position.x = this._refXScale(tileY) + 5;
-            tile.textGraphics.position.y = this._refYScale(tileX) + 5;
+            let tileScaledWidth = this._refXScale(tileY + tileWidth) - this._refXScale(tileY);
+            let tileScaledHeight = this._refYScale(tileX + tileWidth) - this._refYScale(tileX);
+    
+            // add tileScaledHeight / 2 and tileScaledWidth / 2 to center the text on the tile
+            tile.textGraphics.position.x = this._refXScale(tileY) + tileScaledWidth / 2;
+            tile.textGraphics.position.y = this._refYScale(tileX) + tileScaledHeight / 2;
 
-            graphics.drawRect(this._refXScale(tileY), this._refYScale(tileX),
-                              this._refXScale(tileY + tileWidth) - this._refXScale(tileY),
-                              this._refYScale(tileX + tileWidth) - this._refYScale(tileX))
+            graphics.drawRect(this._refXScale(tileY), this._refYScale(tileX), tileScaledWidth, tileScaledHeight);
         } else {
-            tile.textGraphics.position.x = this._refXScale(tileX) + 5;
-            tile.textGraphics.position.y = this._refYScale(tileY) + 5;
+            let tileScaledWidth = this._refXScale(tileX + tileWidth) - this._refXScale(tileX);
+            let tileScaledHeight = this._refYScale(tileY + tileWidth) - this._refYScale(tileY);
 
-            graphics.drawRect(this._refXScale(tileX), this._refYScale(tileY),
-                              this._refXScale(tileX + tileWidth) - this._refXScale(tileX),
-                              this._refYScale(tileY + tileWidth) - this._refYScale(tileY))
+            // add tileScaledHeight / 2 and tileScaledWidth / 2 to center the text on the tile
+            tile.textGraphics.position.x = this._refXScale(tileX) + tileScaledWidth / 2;
+            tile.textGraphics.position.y = this._refYScale(tileY) + tileScaledHeight / 2;;
+
+            graphics.drawRect(this._refXScale(tileX), this._refYScale(tileY), tileScaledWidth, tileScaledHeight);
 
         }
     }
