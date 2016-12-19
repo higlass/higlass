0c0672de6e98e4da13c669e60431c9c0c4066d69
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-12-19 14:40:12 -0500

    Added seriesoptions and config file

---
+++ app/scripts/config.js
@@ -0,0 +1,10 @@
+export const categoryToType = {
+                                '1d-dense': ['line', 'bar', 'point'],
+                                '2d-dense': ['heatmap'],
+                                '1d-axis': ['axis']
+                              };
+
+export const categoryToFiletype = {
+                                '1d-dense': 'hitile',
+                                '2d-dense': 'cooler'
+                              };
