b147b65f06f9c252d99028ca2c26639edca49fee
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-01-13 07:36:20 -0500

    Move images further toward the edge

--- app/scripts/PopupMenu.jsx
+++ app/scripts/PopupMenu.jsx
@@ -25,7 +25,6 @@ export class PopupMenu extends React.Component {
 
 
   componentWillUnmount() {
-    console.log('removing event handler');
     document.removeEventListener('click', this.boundHandleDocumentClick, true);
     window.removeEventListener('resize', this.boundHandleDocumentResize, true);
     ReactDOM.unmountComponentAtNode(this.popup);
