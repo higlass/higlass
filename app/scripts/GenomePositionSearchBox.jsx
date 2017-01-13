e862c45144b56648500722bdd2e22096f37b215e
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-01-12 19:02:26 -0500

    Added menu styling

--- app/scripts/GenomePositionSearchBox.jsx
+++ app/scripts/GenomePositionSearchBox.jsx
@@ -370,7 +370,14 @@ export class GenomePositionSearchBox extends React.Component {
                             position: 'absolute',
                             'left': this.menuPosition.left,
                             'top': this.menuPosition.top,
-                            border: '1px solid black'
+                              borderRadius: '3px',
+                              boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+                              background: 'rgba(255, 255, 255, 0.9)',
+                              padding: '2px 0',
+                              fontSize: '90%',
+                              position: 'fixed',
+                              overflow: 'auto',
+                              maxHeight: '50%' // TODO: don't cheat, let it flow to the bottom
                 }} children={items}/>
                 
                 </PopupMenu>)
