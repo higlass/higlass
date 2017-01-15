414c3fd61653e4a8d38926cd8907a2ec7a5eac5c
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-01-14 19:25:24 -0500

    Don't display heatmap color options by default

--- app/scripts/TiledPlot.jsx
+++ app/scripts/TiledPlot.jsx
@@ -52,7 +52,8 @@ export class TiledPlot extends React.Component {
             addTrackVisible: false,
             addTrackPosition: "top",
             mouseOverOverlayUid: null,
-            trackOptionsUid: 'hm1'
+            //trackOptionsUid: 'hm1'
+            trackOptionsUid: null
         }
 
         // these dimensions are computed in the render() function and depend
