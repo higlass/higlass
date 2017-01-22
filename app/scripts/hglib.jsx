e978c1ba3652d052a17256baa8ac35ce5288008d
Fritz Lekschas <service@lekschas.de>
2017-01-22 15:53:19 -0500

    Load remote config when a string instead of an object is passed as `config`. Also added a demo website to.

--- app/scripts/hglib.jsx
+++ app/scripts/hglib.jsx
@@ -1,8 +1,21 @@
+import { json } from 'd3-request';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HiGlassComponent} from './HiGlassComponent.jsx';
+import { HiGlassComponent } from './HiGlassComponent.jsx';
+
+function launch(parent, config) {
+  ReactDOM.render((<HiGlassComponent viewConfig={config} />), parent);
+}
 
 export function HgComponent(parent, config) {
-    ReactDOM.render(
-            (<HiGlassComponent viewConfig={config} />), parent)
+  if (typeof config === 'string') {
+    // Load external config
+    json(config, (error, data) => {
+      if (error) throw error;
+
+     launch(parent, data);
+    });
+  } else {
+   launch(parent, config);
+  }
 }
