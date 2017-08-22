14a85f17014eba393b6b89389dd06550e6b39a47
Fritz Lekschas <service@lekschas.de>
2017-08-22 19:09:23 -0400

    Set indentation to 2 white spaces

--- app/scripts/Track.js
+++ app/scripts/Track.js
@@ -1,100 +1,91 @@
 import {scaleLinear} from 'd3-scale';
 
 export class Track {
-    constructor() {
-        this._xScale = scaleLinear();
-        this._yScale = scaleLinear();
+  constructor() {
+    this._xScale = scaleLinear();
+    this._yScale = scaleLinear();
 
-        // reference scales used for tracks that can translate and scale
-        // their graphics
-        // They will draw their graphics on the reference scales and then translate
-        // and pan them as needed
-        this._refXScale = scaleLinear();
-        this._refYScale = scaleLinear();
+    // reference scales used for tracks that can translate and scale
+    // their graphics
+    // They will draw their graphics on the reference scales and then translate
+    // and pan them as needed
+    this._refXScale = scaleLinear();
+    this._refYScale = scaleLinear();
 
-        this.position = [0,0];
-        this.dimensions = [1,1];
-        this.options = {};
-    }
+    this.position = [0,0];
+    this.dimensions = [1,1];
+    this.options = {};
+  }
 
-    setDimensions(newDimensions) {
-        this.dimensions = newDimensions;
+  setDimensions(newDimensions) {
+    this.dimensions = newDimensions;
 
-        this._xScale.range([0, this.dimensions[0]]);
-        this._yScale.range([0, this.dimensions[1]]);
-    }
+    this._xScale.range([0, this.dimensions[0]]);
+    this._yScale.range([0, this.dimensions[1]]);
+  }
 
-    refXScale(_) {
-        /**
-         * Either get or set the reference xScale
-         */
-        if (!arguments.length)
-            return this._refXScale;
+  /**
+   * Either get or set the reference xScale
+   */
+  refXScale(_) {
+    if (!arguments.length) return this._refXScale;
 
-        this._refXScale = _;
+    this._refXScale = _;
 
-        return this;
-    }
+    return this;
+  }
 
-    refYScale(_) {
-        /**
-         * Either get or set the reference yScale
-         */
-        if (!arguments.length)
-            return this._refYScale;
+  /**
+   * Either get or set the reference yScale
+   */
+  refYScale(_) {
+    if (!arguments.length) return this._refYScale;
 
-        this._refYScale = _;
+    this._refYScale = _;
 
-        return this;
-    }
+    return this;
+  }
 
-    xScale(_) {
-        /**
-         * Either get or set the xScale
-         */
-        if (!arguments.length)
-            return this._xScale;
+  /**
+   * Either get or set the xScale
+   */
+  xScale(_) {
+    if (!arguments.length) return this._xScale;
 
-        this._xScale = _;
+    this._xScale = _;
 
-        return this;
-    }
+    return this;
+  }
 
-    yScale(_) {
-        /**
-         * Either get or set the yScale
-         */
-        if (!arguments.length)
-            return this._yScale;
+  /**
+   * Either get or set the yScale
+   */
+  yScale(_) {
+    if (!arguments.length)
+      return this._yScale;
 
-        this._yScale = _;
+    this._yScale = _;
 
-        return this;
-    }
+    return this;
+  }
 
-    zoomed(newXScale, newYScale) {
-        this.xScale(newXScale);
-        this.yScale(newYScale);
-    }
+  zoomed(newXScale, newYScale) {
+    this.xScale(newXScale);
+    this.yScale(newYScale);
+  }
 
-    refScalesChanged(refXScale, refYScale) {
-        this._refXScale = refXScale;
-        this._refYScale = refYScale;
-    }
+  refScalesChanged(refXScale, refYScale) {
+    this._refXScale = refXScale;
+    this._refYScale = refYScale;
+  }
 
-    draw() {
+  draw() {}
 
-    }
+  setPosition(newPosition) {
+    this.position = newPosition;
+  }
 
-    setPosition(newPosition) {
-        this.position = newPosition;
-    }
+  remove() {}
 
-    remove() {
-
-    }
-
-    rerender() {
-
-    }
+  rerender() {}
 }
