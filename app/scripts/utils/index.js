67027607b3d4114f607b10cff2244c46c0a203b4
Fritz Lekschas <service@lekschas.de>
2017-08-22 19:08:56 -0400

    Add pixelToGenomeLoci export

--- app/scripts/utils/index.js
+++ app/scripts/utils/index.js
@@ -1,3 +1,4 @@
 export { default as absToChr } from './abs-to-chr';
 export { default as chromInfoBisector } from './chrom-info-bisector';
+export { default as pixelToGenomeLoci } from './pixel-to-genome-loci';
 export { default as scalesToGenomeLoci } from './scales-to-genome-loci';
