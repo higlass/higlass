af78bcf1db5cfa55c9369a7eb5da020c9faf4ef7
Fritz Lekschas <service@lekschas.de>
2017-08-25 12:34:26 -0400

    Avoid invisible class extensions and use TrackControl directly instead

--- app/scripts/CenterTrack.js
+++ app/scripts/CenterTrack.js
@@ -1,4 +1,10 @@
-import FixedTrack from './FixedTrack';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import TrackControl from './TrackControl';
+
+// Styles
+import '../styles/CenterTrack.scss';
 
 const STYLE = {
   opacity: .7,
@@ -6,24 +12,82 @@ const STYLE = {
   position: 'relative'
 };
 
-const EXTENDED_STYLE = Object.assign(STYLE, {
+const EXTENDED_STYLE = Object.assign({}, STYLE, {
   marginRight: '5px'
 })
 
-export default class CenterTrack extends FixedTrack {
-  getCloseImgStyle() {
-    return STYLE
+export default class CenterTrack extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: false
+    };
   }
 
-  getMoveImgStyle() {
-    return EXTENDED_STYLE
+  /* -------------------------- Life Cycle Methods -------------------------- */
+
+  /* ---------------------------- Custom Methods ---------------------------- */
+
+  mouseEnterHandler() {
+    this.setState({
+      isVisible: true
+    });
   }
 
-  getAddImgStyle() {
-    return EXTENDED_STYLE
+  mouseLeaveHandler() {
+    this.setState({
+      isVisible: false
+    });
   }
 
-  getSettingsImgStyle() {
-    return EXTENDED_STYLE
+  /* ------------------------------ Rendering ------------------------------- */
+
+  render() {
+    let classNames = 'center-track';
+
+    classNames += this.props.className ? ` ${this.props.className}`: '';
+
+    return (
+      <div
+        className={classNames}
+        onMouseEnter={this.mouseEnterHandler.bind(this)}
+        onMouseLeave={this.mouseLeaveHandler.bind(this)}
+        style={{
+          height: this.props.height,
+          width: this.props.width
+        }}
+        styleName={classNames}
+      >
+        {this.props.editable &&
+          <TrackControl
+            imgStyleAdd={EXTENDED_STYLE}
+            imgStyleClose={STYLE}
+            imgStyleMove={EXTENDED_STYLE}
+            imgStyleSettings={EXTENDED_STYLE}
+            isMoveable={false}
+            isVisible={this.state.isVisible}
+            onAddSeries={this.props.onAddSeries}
+            onCloseTrackMenuOpened={this.props.onCloseTrackMenuOpened}
+            onConfigTrackMenuOpened={this.props.onConfigTrackMenuOpened}
+            uid={this.props.uid}
+          />
+        }
+      </div>
+    )
   }
+}
+
+CenterTrack.propTypes = {
+  chromInfo: PropTypes.object,
+  className: PropTypes.string,
+  editable: PropTypes.bool,
+  height: PropTypes.number,
+  onAddSeries: PropTypes.func,
+  onCloseTrackMenuOpened: PropTypes.func,
+  onConfigTrackMenuOpened: PropTypes.func,
+  onRangeSelection: PropTypes.func,
+  rangeSelection: PropTypes.array,
+  uid: PropTypes.string,
+  width: PropTypes.number
 }
