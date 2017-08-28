980f961a2567c3499d481cfcf729b01684900ed1
Fritz Lekschas <service@lekschas.de>
2017-08-27 20:45:43 -0400

    CSS Modulefy

--- app/scripts/TrackControl.js
+++ app/scripts/TrackControl.js
@@ -7,24 +7,7 @@ import { SortableHandle } from 'react-sortable-hoc';
 import '../styles/TrackControl.scss';
 
 export class TrackControl extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.Handle = SortableHandle(() => (
-      <svg
-        className="no-zoom"
-        height="10px"
-        onClick={() => {}}
-        style={this.props.imgStyleMove}
-        width="10px"
-      >
-        <use xlinkHref="#move"></use>
-      </svg>
-    ));
-  }
   render() {
-    const Handle = this.Handle;
-
     let className = this.props.isVisible ?
       'track-control-active' : 'track-control';
 
@@ -34,6 +17,21 @@ export class TrackControl extends React.Component {
     className += this.props.isVertical ?
       ' track-control-vertical' : '';
 
+    let buttonClassName = 'track-control-button';
+
+    buttonClassName += this.props.isVertical ?
+      ' track-control-button-vertical' : '';
+
+    const Handle = SortableHandle(() => (
+      <svg
+        className="no-zoom"
+        style={this.props.imgStyleMove}
+        styleName={buttonClassName}
+      >
+        <use xlinkHref="#move"></use>
+      </svg>
+    ));
+
     return (
       <div styleName={className}>
 
@@ -41,7 +39,6 @@ export class TrackControl extends React.Component {
 
         <svg
           className="no-zoom"
-          height="10px"
           onClick={() => {
             let imgDom = ReactDOM.findDOMNode(this.imgConfig);
             let bbox = imgDom.getBoundingClientRect();
@@ -49,25 +46,23 @@ export class TrackControl extends React.Component {
           }}
           ref={(c) => { this.imgConfig = c; }}
           style={this.props.imgStyleSettings}
-          width="10px"
+          styleName={buttonClassName}
         >
           <use xlinkHref="#cog"></use>
         </svg>
 
         <svg
           className="no-zoom"
-          height="10px"
           onClick={() => this.props.onAddSeries(this.props.uid)}
           ref={(c) => { this.imgAdd = c; }}
           style={this.props.imgStyleAdd}
-          width="10px"
+          styleName={buttonClassName}
         >
           <use xlinkHref="#plus"></use>
         </svg>
 
         <svg
           className="no-zoom"
-          height="10px"
           onClick={() => {
             let imgDom = ReactDOM.findDOMNode(this.imgClose);
             let bbox = imgDom.getBoundingClientRect();
@@ -75,7 +70,7 @@ export class TrackControl extends React.Component {
           }}
           ref={(c) => { this.imgClose = c; }}
           style={this.props.imgStyleClose}
-          width="10px"
+          styleName={buttonClassName}
         >
           <use xlinkHref="#cross"></use>
         </svg>
