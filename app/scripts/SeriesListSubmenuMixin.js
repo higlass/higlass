58980ce4deac6512e72a94f7fd40534e380accd2
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-01-08 16:04:02 -0500

    Added the SeriesListSubmenuMixin

---
+++ app/scripts/SeriesListSubmenuMixin.js
@@ -0,0 +1,64 @@
+import {Mixin} from 'mixwith';
+
+import { SeriesListMenu } from './SeriesListMenu';
+import { getAllTracksAndSubtracks } from './SeriesListItems';
+
+export const SeriesListSubmenuMixin = Mixin((superclass) => class extends superclass {
+  getSubmenu() {
+    if (this.state.submenuShown) {
+      // the bounding box of the element which initiated the subMenu
+      // necessary so that we can position the submenu next to the initiating
+      // element
+      const bbox = this.state.submenuSourceBbox;
+      let position = null;
+
+      if (this.state.orientation == 'left') {
+        position = {
+          left: this.state.left,
+          top: bbox.top,
+        };
+      } else {
+        position = {
+          left: this.state.left + bbox.width + 7,
+          top: bbox.top,
+        };
+      }
+
+      const series = getAllTracksAndSubtracks(this.props.tracks);
+      console.log('series:', series);
+      const selectedTrack = series.filter(t => t.uid == this.state.submenuShown.uid)[0];
+
+      // for now we can't replace 'whole' tracks because they're
+      // just the horizontal and vertical rule tracks
+      const canBeReplaced = !(selectedTrack.position == 'whole');
+      console.log('selectedTrack:', selectedTrack);
+      console.log('canBeReplaced:', canBeReplaced);
+
+
+      console.log('selectedTrack:', selectedTrack);
+
+      return (
+        <SeriesListMenu
+          closeMenu={this.props.closeMenu}
+          hostTrack={this.props.tracks[0]}
+          onAddSeries={this.props.onAddSeries}
+          onChangeTrackType={this.props.onChangeTrackType}
+          onCloseTrack={() => this.props.onCloseTrack(this.state.submenuShown.uid)}
+          onConfigureTrack={this.props.onConfigureTrack}
+          onExportData={this.props.onExportData}
+          onLockScales={this.props.onLockScales}
+          onTrackOptionsChanged={this.props.onTrackOptionsChanged}
+          onDivideSeries={this.props.onDivideSeries}
+          orientation={this.state.orientation}
+          ref={c => this.seriesListMenu = c}
+          parentBbox={bbox}
+          position={position}
+          series={this.state.submenuShown}
+          track={selectedTrack}
+          trackOrientation={this.props.trackOrientation}
+        />
+      );
+    }
+    return (<div />);
+  }
+});
