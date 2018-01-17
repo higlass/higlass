1968f28141faeda1915aca56beacbbaf45d73a54
Fritz Lekschas <service@lekschas.de>
2018-01-17 15:38:29 -0500

    Do not move cooled down insets. Reheat insets on scale change

--- app/scripts/Combined2dAnnotationsTrack.js
+++ app/scripts/Combined2dAnnotationsTrack.js
@@ -132,7 +132,8 @@ class Combined2dAnnotationsTrack {
   positionInsets() {
     if (!this.insetsToBeDrawn.length) return [];
 
-    const ancs = this.drawnAnnotations.map(obj => ({
+    const anchors = this.drawnAnnotations.map(obj => ({
+      t: 1,
       x: (obj.maxX + obj.minX) / 2,
       y: (obj.maxY + obj.minY) / 2,
       ox: (obj.maxX + obj.minX) / 2,  // Origin x
@@ -142,57 +143,79 @@ class Combined2dAnnotationsTrack {
       ...obj
     }));
 
-    const labs = this.insetsToBeDrawn.map((obj) => {
-      if (!this.insets[obj.uid]) {
-        this.insets[obj.uid] = {
-          x: (obj.maxX + obj.minX) / 2,
-          y: (obj.maxY + obj.minY) / 2,
-          ox: (obj.maxX + obj.minX) / 2,  // Origin x
-          oy: (obj.maxY + obj.minY) / 2,  // Origin y
-          width: 64,
-          height: 64,
-          wh: 32,  // Width half
-          hh: 32,  // Heigth half
-          ...obj
-        };
-      } else {
-        const newOx = (obj.maxX + obj.minX) / 2;
-        const newOy = (obj.maxY + obj.minY) / 2;
-        const dX = this.insets[obj.uid].ox - newOx;
-        const dY = this.insets[obj.uid].oy - newOy;
+    const insets = this.insetsToBeDrawn
+      .map((inset) => {
+        if (!this.insets[inset.uid]) {
+          // Add new inset
+          this.insets[inset.uid] = {
+            t: 1.0,
+            x: (inset.maxX + inset.minX) / 2,
+            y: (inset.maxY + inset.minY) / 2,
+            ox: (inset.maxX + inset.minX) / 2,  // Origin x
+            oy: (inset.maxY + inset.minY) / 2,  // Origin y
+            width: 64,
+            height: 64,
+            wh: 32,  // Width half
+            hh: 32,  // Heigth half
+            ...inset
+          };
+        } else {
+          // Update existing inset positions
+          const newOx = (inset.maxX + inset.minX) / 2;
+          const newOy = (inset.maxY + inset.minY) / 2;
+          const dX = this.insets[inset.uid].ox - newOx;
+          const dY = this.insets[inset.uid].oy - newOy;
 
-        this.insets[obj.uid].ox = newOx;
-        this.insets[obj.uid].oy = newOy;
+          this.insets[inset.uid].ox = newOx;
+          this.insets[inset.uid].oy = newOy;
 
-        this.insets[obj.uid].x += dX;
-        this.insets[obj.uid].y += dY;
-      }
+          this.insets[inset.uid].x -= dX;
+          this.insets[inset.uid].y -= dY;
 
-      return this.insets[obj.uid];
-    });
+          this.insets[inset.uid].t = this.scaleChanged ? 0.5 : 0;
+        }
 
-    const t0 = performance.now();
-    positionLabels
-      .label(labs) // insets
-      .anchor(ancs) // Collisions
-      .width(this.dimensions[0])
-      .height(this.dimensions[1])
-      .start(Math.max(2, Math.min(100 / this.insetsToBeDrawn.length)));
+        return this.insets[inset.uid];
+      });
 
-    // console.log('Labeling took ' + (performance.now() - t0) + ' msec');
+    const insetsToBePositioned = insets
+      .filter((inset) => {
+        if (inset.t) return true;
 
-    const pos = labs.map(lab => ([
-      lab.uid,
-      lab.x,
-      lab.y,
-      lab.width,
-      lab.height,
-      lab.ox,
-      lab.oy,
-      lab.cX1,
-      lab.cX2,
-      lab.cY1,
-      lab.cY2
+        // Inset has cooled down (i.e., is already positions), hence, it is
+        // filtered out and added to anchors instead.
+        anchors.push(inset);
+        return false;
+      });
+
+    if (insetsToBePositioned.length) {
+      const t0 = performance.now();
+
+      positionLabels
+        // Insets, i.e., labels
+        .label(insetsToBePositioned)
+        // Anchors, i.e., label origins, already positioned labels, and other
+        // annotations
+        .anchor(anchors)
+        .width(this.dimensions[0])
+        .height(this.dimensions[1])
+        .start(Math.max(2, Math.min(100 / insetsToBePositioned.length)));
+
+      console.log(`Labeling took ${performance.now() - t0} msec`);
+    }
+
+    const pos = insets.map(inset => ([
+      inset.uid,
+      inset.x,
+      inset.y,
+      inset.width,
+      inset.height,
+      inset.ox,
+      inset.oy,
+      inset.cX1,
+      inset.cX2,
+      inset.cY1,
+      inset.cY2
     ]));
 
     return pos;
@@ -213,7 +236,9 @@ class Combined2dAnnotationsTrack {
     newContents.forEach((nc) => {
       currentTracks.add(nc.uid);
 
-      if (nc.uid in this.createdTracks) { newTracks.push(this.createdTracks[nc.uid]); } else {
+      if (nc.uid in this.createdTracks) {
+        newTracks.push(this.createdTracks[nc.uid]);
+      } else {
         const newTrack = trackCreator(nc);
         newTrack.setPosition(this.position);
         newTrack.setDimensions(this.dimensions);
