36ac237c51f92974b5a6ecca51ae13b763f41999
Fritz Lekschas <service@lekschas.de>
2018-01-24 22:15:23 -0500

    Dynamically choose size based on base64 image by default

--- app/scripts/utils/base64-to-canvas.js
+++ app/scripts/utils/base64-to-canvas.js
@@ -1,15 +1,12 @@
-const base64ToCanvas = (base64, w, h) => {
+const base64ToCanvas = (base64, width, height) => {
   const canvas = document.createElement('canvas');
-
-  canvas.width = w;
-  canvas.height = h;
-
-  const ctx = canvas.getContext('2d');
 
   return new Promise((resolve, reject) => {
     const img = new window.Image();
     img.onload = () => {
-      ctx.drawImage(img, 0, 0);
+      canvas.width = width || img.width;
+      canvas.height = height || img.height;
+      canvas.getContext('2d').drawImage(img, 0, 0);
       resolve(canvas);
     };
 
