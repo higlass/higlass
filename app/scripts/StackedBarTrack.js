c54c24668dbc7648671a23e14ba76fb3f28d7955
Fritz Lekschas <service@lekschas.de>
2018-02-07 10:50:48 -0500

    Massive ESLintify

--- app/scripts/StackedBarTrack.js
+++ app/scripts/StackedBarTrack.js
@@ -1,13 +1,11 @@
-import {BarTrack} from './BarTrack';
-import {scaleLinear, scaleOrdinal, schemeCategory10} from 'd3-scale';
-import {range} from 'd3-array';
-import {colorToHex} from './utils';
+import { range } from 'd3-array';
+import { scaleLinear, scaleOrdinal, schemeCategory10 } from 'd3-scale';
 
-export class StackedBarTrack extends BarTrack {
-  constructor(scene, dataConfig, handleTilesetInfoReceived, option, animate, onValueScaleChanged) {
-    super(scene, dataConfig, handleTilesetInfoReceived, option, animate, onValueScaleChanged);
-  }
+import BarTrack from './BarTrack';
 
+import { colorToHex } from './utils';
+
+export default class StackedBarTrack extends BarTrack {
   initTile(tile) {
     this.renderTile(tile);
   }
@@ -17,35 +15,45 @@ export class StackedBarTrack extends BarTrack {
     tile.drawnAtScale = this._xScale.copy();
 
     // we're setting the start of the tile to the current zoom level
-    const {tileX, tileWidth} = this.getTilePosAndDimensions(tile.tileData.zoomLevel,
-      tile.tileData.tilePos, this.tilesetInfo.tile_size);
+    const { tileX, tileWidth } = this.getTilePosAndDimensions(
+      tile.tileData.zoomLevel,
+      tile.tileData.tilePos,
+      this.tilesetInfo.tile_size
+    );
 
-    const shapeX = tile.tileData.shape[0]; // number of different nucleotides in each bar
-    const shapeY = tile.tileData.shape[1]; // number of bars
-    let flattenedArray = tile.tileData.dense;
+    // number of different nucleotides in each bar
+    const shapeX = tile.tileData.shape[0];
+    // number of bars
+    const shapeY = tile.tileData.shape[1];
+    const flattenedArray = tile.tileData.dense;
 
     // if any data is negative, switch to exponential scale
-    if (flattenedArray.filter((a) => a < 0).length > 0 && this.options.valueScaling === 'linear') {
-      console.warn('Negative values present in data. Defaulting to exponential scale.');
+    if (
+      flattenedArray.filter(a => a < 0).length > 0 &&
+      this.options.valueScaling === 'linear'
+    ) {
+      console.warn(
+        'Negative values present in data. Defaulting to exponential scale.'
+      );
       this.options.valueScaling = 'exponential';
       // todo does anything meaningful even happen here
     }
 
-    // un-flatten data into matrix of tile.tileData.shape[0] x tile.tileData.shape[1]
-    // first array in the matrix will be [flattenedArray[0], flattenedArray[256], flattenedArray[512], etc.]
+    // un-flatten data into matrix of:
+    //  tile.tileData.shape[0] x tile.tileData.shape[1]
+    // first array in the matrix will be
+    //  [flattenedArray[0], flattenedArray[256], flattenedArray[512], etc.]
     // because of how flattenedArray comes back from the server.
     const matrix = [];
-    for (let i = 0; i < shapeX; i++) {//6
-      for (let j = 0; j < shapeY; j++) {//256;
-        let singleBar;
-        (matrix[j] === undefined) ? singleBar = [] : singleBar = matrix[j];
+    for (let i = 0; i < shapeX; i++) {  // 6
+      for (let j = 0; j < shapeY; j++) {  // 256
+        const singleBar = matrix[j] === undefined ? [] : matrix[j];
         singleBar.push(flattenedArray[(shapeY * i) + j]);
         matrix[j] = singleBar;
       }
     }
 
     if (this.options.scaledHeight === true) {
-
       // find max height of bars for scaling in the track
       let positiveMax = 0;
       let negativeMax = 0;
@@ -54,20 +62,28 @@ export class StackedBarTrack extends BarTrack {
 
         // find total heights of each positive column and each negative column
         // and compare to highest overall values above
-        const localPositiveMax = temp.filter((a) => a >= 0).reduce((a, b) => a + b, 0);
-        (localPositiveMax > positiveMax) ? positiveMax = localPositiveMax : positiveMax;
-        const localNegativeMax = Math.abs(temp.filter((a) => a < 0).reduce((a, b) => a + b, 0));
-        (localNegativeMax > negativeMax) ? negativeMax = localNegativeMax : negativeMax;
-
+        const localPositiveMax = temp
+          .filter(a => a >= 0)
+          .reduce((a, b) => a + b, 0);
+        positiveMax = localPositiveMax > positiveMax
+          ? localPositiveMax
+          : positiveMax;
+        const localNegativeMax = Math.abs(
+          temp.filter(a => a < 0).reduce((a, b) => a + b, 0)
+        );
+        negativeMax = localNegativeMax > negativeMax
+          ? localNegativeMax
+          : negativeMax;
       }
-      this.drawVerticalBars(graphics, matrix, tileX, tileWidth, positiveMax, negativeMax);
-    }
-    else {
+      this.drawVerticalBars(
+        graphics, matrix, tileX, tileWidth, positiveMax, negativeMax
+      );
+    } else {
       // normalize each array in matrix
       for (let i = 0; i < matrix.length; i++) {
         const temp = matrix[i];
         const barValuesSum = temp.reduce((a, b) => a + b, 0);
-        matrix[i] = temp.map((a) => a / barValuesSum);
+        matrix[i] = temp.map(a => a / barValuesSum);
       }
       this.drawNormalizedBars(graphics, matrix, tileX, tileWidth);
     }
@@ -75,15 +91,18 @@ export class StackedBarTrack extends BarTrack {
 
   /**
    * Draws graph using normalized values.
-   *
    * @param graphics PIXI.Graphics instance
    * @param matrix 2d array of numbers representing nucleotides
    * @param tileX starting position of tile
    * @param tileWidth pre-scaled width of tile
-   * @param positiveMax the height of the tallest bar in the positive part of the graph
-   * @param negativeMax the height of the tallest bar in the negative part of the graph
+   * @param positiveMax the height of the tallest bar in the positive part of
+   *   the graph
+   * @param negativeMax the height of the tallest bar in the negative part of
+   *   the graph
    */
-  drawVerticalBars(graphics, matrix, tileX, tileWidth, positiveMax, negativeMax) {
+  drawVerticalBars(
+    graphics, matrix, tileX, tileWidth, positiveMax, negativeMax
+  ) {
     graphics.clear();
     const trackHeight = this.dimensions[1];
 
@@ -94,7 +113,7 @@ export class StackedBarTrack extends BarTrack {
 
     const colorScale = scaleOrdinal()
       .range(this.options.colorScale)
-      .domain(range(this.options.colorScale.length)) 
+      .domain(range(this.options.colorScale.length))
       || scaleOrdinal(schemeCategory10);
     const valueToPixels = scaleLinear()
       .domain([0, positiveMax])
@@ -102,36 +121,53 @@ export class StackedBarTrack extends BarTrack {
     let prevStackedBarHeight = 0;
 
     for (let j = 0; j < matrix.length; j++) { // jth vertical bar in the graph
-      const x = this._xScale(tileX + (j * tileWidth / this.tilesetInfo.tile_size));
-      const width = this._xScale(tileX + (tileWidth / this.tilesetInfo.tile_size)) - this._xScale(tileX);
+      const x = this._xScale(
+        tileX + (j * tileWidth / this.tilesetInfo.tile_size)
+      );
+      const width = this._xScale(
+        tileX + (tileWidth / this.tilesetInfo.tile_size)
+      ) - this._xScale(tileX);
 
       // sorted from largest to smallest
       // we're grouping values along with their index in the column
-      const positiveValsSorted = matrix[j].map((x,i) => [x,i]).filter((a) => a[0] >= 0).sort((a, b) => a[0] - b[0]);
+      const positiveValsSorted = matrix[j]
+        .map((_x, i) => [_x, i])
+        .filter(a => a[0] >= 0)
+        .sort((a, b) => a[0] - b[0]);
 
       for (let i = 0; i < positiveValsSorted.length; i++) {
         const height = valueToPixels(positiveValsSorted[i][0]);
-        const y = (trackHeight - negativeTrackHeight) - (prevStackedBarHeight + height);
+        const y = (
+          (trackHeight - negativeTrackHeight)
+          - (prevStackedBarHeight + height)
+        );
         graphics.beginFill(colorToHex(colorScale(positiveValsSorted[i][1])));
         graphics.drawRect(x, y, width, height);
-        prevStackedBarHeight = prevStackedBarHeight + height;
+        prevStackedBarHeight += height;
       }
       prevStackedBarHeight = 0;
     }
 
     for (let j = 0; j < matrix.length; j++) { // jth vertical bar in the graph
-      const x = this._xScale(tileX + (j * tileWidth / this.tilesetInfo.tile_size));
-      const width = this._xScale(tileX + (tileWidth / this.tilesetInfo.tile_size)) - this._xScale(tileX);
+      const x = this._xScale(
+        tileX + (j * tileWidth / this.tilesetInfo.tile_size)
+      );
+      const width = this._xScale(
+        tileX + (tileWidth / this.tilesetInfo.tile_size)
+      ) - this._xScale(tileX);
       // sorted from largest to smallest
       // we're grouping values along with their index in the column
-      const negativeValsSorted = matrix[j].map((x,i) => [x,i]).filter((a) => a[0] < 0).sort((a, b) => a[0] - b[0]);
+      const negativeValsSorted = matrix[j]
+        .map((_x, i) => [_x, i])
+        .filter(a => a[0] < 0)
+        .sort((a, b) => a[0] - b[0]);
 
       for (let i = 0; i < negativeValsSorted.length; i++) {
         const height = valueToPixels(negativeValsSorted[i][0]);
         const y = positiveTrackHeight - (prevStackedBarHeight + height);
         graphics.beginFill(colorToHex(colorScale(negativeValsSorted[i][1])));
         graphics.drawRect(x, y, width, height);
-        prevStackedBarHeight = prevStackedBarHeight - height;
+        prevStackedBarHeight -= height;
       }
       prevStackedBarHeight = 0;
     }
@@ -155,8 +191,12 @@ export class StackedBarTrack extends BarTrack {
     let prevStackedBarHeight = 0;
 
     for (let j = 0; j < matrix.length; j++) { // jth vertical bar in the graph
-      const x = this._xScale(tileX + (j * tileWidth / this.tilesetInfo.tile_size));
-      const width = this._xScale(tileX + (tileWidth / this.tilesetInfo.tile_size)) - this._xScale(tileX);
+      const x = this._xScale(
+        tileX + (j * tileWidth / this.tilesetInfo.tile_size)
+      );
+      const width = this._xScale(
+        tileX + (tileWidth / this.tilesetInfo.tile_size)
+      ) - this._xScale(tileX);
 
       // mapping each value to its original color. color: value
       const rowWithOriginalIndices = {};
@@ -164,9 +204,7 @@ export class StackedBarTrack extends BarTrack {
         rowWithOriginalIndices[colorToHex(colorScale(i))] = matrix[j][i];
       }
 
-      const sorted = matrix[j].sort((a, b) => {
-        return b - a;
-      });
+      const sorted = matrix[j].sort((a, b) => b - a);
       for (let i = 0; i < sorted.length; i++) {
         const y = this.position[0] + (prevStackedBarHeight * trackHeight);
         const height = valueToPixels(sorted[i]);
@@ -177,15 +215,9 @@ export class StackedBarTrack extends BarTrack {
           }
         }
         graphics.drawRect(x, y, width, height);
-        prevStackedBarHeight = prevStackedBarHeight + sorted[i];
+        prevStackedBarHeight += sorted[i];
       }
       prevStackedBarHeight = 0;
     }
   }
-
-  draw() {
-    super.draw();
-  }
 }
-
-export default StackedBarTrack;
