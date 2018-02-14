0d30838165433aa6e390d6933ca8cc7fee00f9df
Fritz Lekschas <service@lekschas.de>
2018-02-14 00:06:59 -0500

    Replace `map` with `translate`. Map does not make sense in the context of sets.

--- app/scripts/factories/KeySet.js
+++ app/scripts/factories/KeySet.js
@@ -7,14 +7,14 @@
  * @param  {Array}  items  Items to be added.
  */
 const KeySet = function KeySet(keyProp = 'id', items = []) {
-  items.forEach((item) => { this.add(item); });
-
   const store = {};
   const getStore = () => store;
   const getKeyProp = () => keyProp;
 
   Object.defineProperty(this, '_keyProp', { get: getKeyProp });
   Object.defineProperty(this, '_store', { get: getStore });
+
+  items.forEach((item) => { this.add(item); });
 };
 
 /* ------------------------------- Properties ------------------------------- */
@@ -50,9 +50,10 @@ KeySet.prototype[Symbol.iterator] = function* iterator() {
 };
 
 KeySet.prototype.add = function add(item) {
-  if (!item || !item[this._keyProp]) return;
+  if (!item || !item[this._keyProp]) return false;
 
   this._store[item[this._keyProp]] = item;
+  return true;
 };
 
 KeySet.prototype.clear = function clear() {
@@ -61,7 +62,6 @@ KeySet.prototype.clear = function clear() {
     delete this._store[key];
   });
 };
-
 
 KeySet.prototype.clone = function clone() {
   return new KeySet(this._keyProp, this.values);
@@ -99,18 +99,21 @@ KeySet.prototype.has = function has(item) {
   return !!this._store[item[this._keyProp]];
 };
 
-KeySet.prototype.map = function map(f) {
-  const newKeySet = this.clone();
-  this.keys.forEach((key) => { newKeySet.add(f(this._store[key])); });
-  return newKeySet;
-};
-
 KeySet.prototype.reduce = function reduce(f, initial) {
   return this.values.reduce(f, initial);
 };
 
 KeySet.prototype.some = function some(f) {
   return this.values.some(f);
+};
+
+/**
+ * Translate the items of the set to an Array of new form based on the given translator.
+ * @param   {Function}  f  Translator function being applied on every item of the set.
+ * @return  {Array}  List of translated items.
+ */
+KeySet.prototype.translate = function translate(f) {
+  return this.values.map(value => f(value));
 };
 
 
