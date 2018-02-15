d4e5840e13e82f6b61bdef5c97c6ddb72f796d40
Fritz Lekschas <service@lekschas.de>
2018-02-15 18:52:32 -0500

    Allow disabling transparent color

--- app/scripts/utils/color-domain-to-rgba-array.js
+++ app/scripts/utils/color-domain-to-rgba-array.js
@@ -7,21 +7,24 @@ import { rgb } from 'd3-color';
  * values (all from 0 to 255). The last color (255) will always be
  * transparent
  */
-const colorDomainToRgbaArray = (colorRange) => {
+const colorDomainToRgbaArray = (colorRange, noTansp = false) => {
   // we should always have at least two values in the color range
   const domain = colorRange.map((x, i) => i * (255 / (colorRange.length - 1)));
 
-  const d3Scale = scaleLinear().domain(domain)
-    .range(colorRange);
+  const d3Scale = scaleLinear().domain(domain).range(colorRange);
 
-  const rgbaArray = range(254, -1, -1).map(d3Scale)
+  const fromX = noTansp ? 255 : 254;
+
+  const rgbaArray = range(fromX, -1, -1).map(d3Scale)
     .map((x) => {
       const r = rgb(x);
       return [r.r, r.g, r.b, 255];
     });
 
-  // add a transparent color at the end for missing values
-  rgbaArray.push([255, 255, 255, 0]);
+  // add a transparent color at the end for missing values and, more
+  // importantly, non-existing values such as the empty upper right or lower
+  // left triangle of tiles on the diagonal.
+  if (rgbaArray.length < 256) rgbaArray.push([255, 255, 255, 0]);
 
   return rgbaArray;
 };
