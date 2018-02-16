5f7b26fff24fff574ff18dc827c89bb5c076fb5d
Fritz Lekschas <service@lekschas.de>
2018-02-15 21:57:47 -0500

    Avoid data duplication and add refresh function

--- app/scripts/factories/LabelCluster.js
+++ app/scripts/factories/LabelCluster.js
@@ -13,6 +13,30 @@ LabelCluster.prototype.constructor = LabelCluster;
 
 /* ------------------------------- Properties ------------------------------- */
 
+function getMinX() {
+  return this.src.minX;
+}
+
+Object.defineProperty(LabelCluster.prototype, 'minX', { get: getMinX });
+
+function getMaxX() {
+  return this.src.maxX;
+}
+
+Object.defineProperty(LabelCluster.prototype, 'maxX', { get: getMaxX });
+
+function getMinY() {
+  return this.src.minY;
+}
+
+Object.defineProperty(LabelCluster.prototype, 'minY', { get: getMinY });
+
+function getMaxY() {
+  return this.src.maxY;
+}
+
+Object.defineProperty(LabelCluster.prototype, 'maxY', { get: getMaxY });
+
 function getDataPos() {
   return this.src.members.translate(member => member.getDataPosition());
 }
@@ -33,14 +57,14 @@ LabelCluster.prototype.compDimPos = function compDimPos() {
   this.oY = this.y;
 
   // Update bounding area of the origins
-  this.minX = this.src.members
-    .reduce((a, member) => Math.min(a, member.minX), Infinity);
-  this.maxX = this.src.members
-    .reduce((a, member) => Math.max(a, member.maxX), -Infinity);
-  this.minY = this.src.members
-    .reduce((a, member) => Math.min(a, member.minY), Infinity);
-  this.maxY = this.src.members
-    .reduce((a, member) => Math.max(a, member.maxY), -Infinity);
+  // this.minX = this.src.members
+  //   .reduce((a, member) => Math.min(a, member.minX), Infinity);
+  // this.maxX = this.src.members
+  //   .reduce((a, member) => Math.max(a, member.maxX), -Infinity);
+  // this.minY = this.src.members
+  //   .reduce((a, member) => Math.min(a, member.minY), Infinity);
+  // this.maxY = this.src.members
+  //   .reduce((a, member) => Math.max(a, member.maxY), -Infinity);
 
   // Update width and height halfs of the bounding area of the origin
   this.oWH = (this.maxX - this.minX) / 2;
@@ -60,6 +84,17 @@ LabelCluster.prototype.setDim = function setDim(width, height) {
 LabelCluster.prototype.setSrc = function setSrc(cluster) {
   this.src = cluster;
   this.compDimPos();
+  return this;
+};
+
+LabelCluster.prototype.refresh = function refresh() {
+  this.src.refresh();
+
+  this.oX = this.src.cX;
+  this.oY = this.src.cY;
+  this.oWH = (this.maxX - this.minX) / 2;
+  this.oHH = (this.maxY - this.minY) / 2;
+
   return this;
 };
 
