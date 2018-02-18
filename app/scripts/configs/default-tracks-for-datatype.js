ef9346001594bae422b8694689229781da28f0e2
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-02-17 22:19:51 -0500

    Add default-tracks-for-datatype

---
+++ app/scripts/configs/default-tracks-for-datatype.js
@@ -0,0 +1,21 @@
+export const DEFAULT_TRACKS_FOR_DATATYPE = {
+  'matrix': {
+    'center': 'heatmap',
+    'top': 'horizontal-heatmap',
+    'left': 'vertical-heatmap',
+    'right': 'vertical-heatmap',
+    'bottom': 'horizontal-heatmap',
+  },
+  'vector': {
+    'top': 'horizontal-bar',
+    'bottom': 'horizontal-bar',
+    'left': 'vertical-bar',
+    'right': 'vertical-bar',
+  },
+  'multivec': {
+    'top': 'horizontal-multivec',
+    'bottom': 'horizontal-multivec',
+  },
+};
+
+export default DEFAULT_TRACKS_FOR_DATATYPE;
