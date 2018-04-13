7875f92e772367ac24ec6928bc0deaa314c09b2e
Fritz Lekschas <service@lekschas.de>
2018-04-13 10:43:24 -0400

    Add documentation

--- app/scripts/services/is-dark-theme.js
+++ app/scripts/services/is-dark-theme.js
@@ -1,5 +1,13 @@
 let isDarkTheme = false;
 
+/**
+ * Determine if the dark theme is activated
+ * @return  {boolean}  If `true` the dark theme is activated
+ */
 export const getDarkTheme = () => isDarkTheme;
 
+/**
+ * Activate or deactivate the dark them
+ * @param   {boolean}  unset  If `true` deactivate the dark theme.
+ */
 export const setDarkTheme = (unset = false) => { isDarkTheme = !unset; };
