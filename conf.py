7e8f579289c0a0902c2d02f93ebcb005d2299238
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-05-21 13:22:45 -0400

    Added add horizontal cross section section

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'HiGlass'
-copyright = '2017, Peter Kerpedjiev'
+copyright = '2017,2018 HiGlass Developers'
 author = 'Peter Kerpedjiev'
 
 # The version info for the project you're documenting, acts as replacement for
