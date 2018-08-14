38d5bafa007d1e881c1e94714c51ccf5e407abf3
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-08-14 11:18:49 -0400

    stub test

---
+++ test/fetch_test.js
@@ -0,0 +1,12 @@
+import { fetchEither } from '../app/scripts/services/tile-proxy.js';
+
+import { expect } from 'chai';
+
+
+describe('fetchEither', function() {
+
+	it('should be a function', function() {
+		expect(typeof(fetchEither)).to.eql('function??');
+	});
+
+});
