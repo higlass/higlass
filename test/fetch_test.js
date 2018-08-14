8cc39637617dccd5120ef83bc86022bb00b2c6a9
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-08-14 15:01:01 -0400

    Test text queries

--- test/fetch_test.js
+++ test/fetch_test.js
@@ -36,14 +36,19 @@ describe('tile-proxy text', function() {
 		expect(typeof(api.json)).to.eql('function');
 	});
 
-/* Times out */
-//   it('handles text', function(done) {
-//     api.text(
-//       'http://higlass.io/api/v1/available-chrom-sizes/',
-//       function(status, text_response) {
-//         expect(text_response).to.eql('asdf!');
-//         done();
-//       });
-//   });
+  /* 
+  Failed assertions in the callback are caught inside fetchEither,
+  instead of failing out immediately, and the ultimate test failure
+  is an uninformative timeout. Perhaps get rid of our catch?
+  */
+  it('handles text', function(done) {
+    api.text(
+      'http://higlass.io/api/v1/available-chrom-sizes/',
+      function(status, text_response) {
+        expect(text_response).to.have.string('count');
+        expect(text_response).to.have.string('results');
+        done();
+      });
+  });
 
 });
