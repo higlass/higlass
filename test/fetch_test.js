2e09abe27cd0b5bf0865ca276f28504277d7c9b3
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-08-14 12:02:13 -0400

    test that fetch utils are exported

--- test/fetch_test.js
+++ test/fetch_test.js
@@ -1,12 +1,16 @@
-import { fetchEither } from '../app/scripts/services/tile-proxy.js';
+import api from '../app/scripts/services/tile-proxy';
 
 import { expect } from 'chai';
 
 
-describe('fetchEither', function() {
+describe('tile-proxy fetch helper', function() {
 
-	it('should be a function', function() {
-		expect(typeof(fetchEither)).to.eql('function??');
+	it('should have json function', function() {
+		expect(typeof(api.json)).to.eql('function');
+	});
+
+	it('should have text function', function() {
+		expect(typeof(api.json)).to.eql('function');
 	});
 
 });
