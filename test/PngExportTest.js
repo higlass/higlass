dd53470673734be7e4f25898e23d7d55965f52b4
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-10-26 14:02:33 -0400

    Actually test PNG export Change one test to a before each, which seems more appropriate

--- test/PngExportTest.js
+++ test/PngExportTest.js
@@ -2,110 +2,66 @@
 import {
   configure,
   mount,
-  // render,
-  ReactWrapper,
 } from 'enzyme';
 
 import Adapter from 'enzyme-adapter-react-16';
 
 import { expect } from 'chai';
-import { select } from 'd3-selection';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import slugid from 'slugid';
 
 import HiGlassComponent from '../app/scripts/HiGlassComponent';
-import HeatmapOptions from '../app/scripts/HeatmapOptions';
 
 // Utils
 import {
-  scalesCenterAndK,
-  totalTrackPixelHeight,
-  getTrackByUid,
-  getTrackObjectFromHGC,
-  getTiledPlot,
-  waitForJsonComplete,
   waitForTilesLoaded,
-  waitForTransitionsFinished,
 } from '../app/scripts/utils';
 
 // View configs
 import {
-  // paperFigure1,
-  osmConf,
-  geneAnnotationsOnly,
-  geneAnnotationsOnly1,
-  annotationsTilesView,
-  horizontalAndVerticalMultivec,
-  exportDataConfig,
-  invalidTrackConfig,
-  divergentTrackConfig,
-  divisionViewConfig,
-  simpleCenterViewConfig,
-  rectangleDomains,
-  threeViews,
-  project1D,
-  noGPSB,
-  onlyGPSB,
-  chromInfoTrack,
-  heatmapTrack,
-  twoViewConfig,
-  oneViewConfig,
-  oneTrackConfig,
-  oneZoomedOutViewConf,
-  // valueIntervalTrackViewConf,
-  horizontalDiagonalTrackViewConf,
-  horizontalHeatmapTrack,
-  largeHorizontalHeatmapTrack,
-  verticalHeatmapTrack,
-  chromosomeGridTrack,
   testViewConfX1,
-  testViewConfX2,
 } from './view-configs';
 
 configure({ adapter: new Adapter() });
 
-describe('Simple HiGlassComponent', () => {
+describe('PNG Export', () => {
   let hgc = null;
   let div = null;
+  
+  beforeEach((done) => {
+    if (hgc) {
+      hgc.unmount();
+      hgc.detach();
+    }
 
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    if (div) {
+      global.document.body.removeChild(div);
+    }
 
-  describe('Export SVG properly', () => {
-    it('Cleans up previously created instances and mounts a new component', (done) => {
-      if (hgc) {
-        hgc.unmount();
-        hgc.detach();
-      }
+    div = global.document.createElement('div');
+    global.document.body.appendChild(div);
 
-      if (div) {
-        global.document.body.removeChild(div);
-      }
+    div.setAttribute('style', 'width:800px;background-color: lightgreen');
+    div.setAttribute('id', 'simple-hg-component');
 
-      div = global.document.createElement('div');
-      global.document.body.appendChild(div);
+    hgc = mount(<HiGlassComponent
+      options={{ bounded: false }}
+      viewConfig={testViewConfX1}
+    />, { attachTo: div });
 
-      div.setAttribute('style', 'width:800px;background-color: lightgreen');
-      div.setAttribute('id', 'simple-hg-component');
+    hgc.update();
+    waitForTilesLoaded(hgc.instance(), done);
+  });
 
-      hgc = mount(<HiGlassComponent
-        options={{ bounded: false }}
-        viewConfig={testViewConfX1}
-      />, { attachTo: div });
-
-      hgc.update();
-      waitForTilesLoaded(hgc.instance(), done);
-    });
-
-    it ("Exports to SVG", (done) => {
-      let svg = hgc.instance().createSVG();
-      let svgText = new XMLSerializer().serializeToString(svg);
-
-      expect(svgText.indexOf('rect')).to.be.above(0);
-      // hgc.instance().handleExportSVG();
-      //
-
-      done();
+  it ("Exports to PNG", (done) => {
+    const blobPromise = hgc.instance().createPNGBlobPromise();
+    blobPromise.then(function(blob) {
+      const reader = new FileReader();
+      reader.addEventListener("loadend", function() {
+        const array = new Uint8Array(reader.result.slice(1,4));
+        const pngString = new TextDecoder("iso-8859-2").decode(array);
+        expect(pngString).to.equal('PNG');
+        done();
+      });
+      reader.readAsArrayBuffer(blob);
     });
   });
 });
