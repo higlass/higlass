d573f6c9470a033bce0c5beff30968dce4acf7c9
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-11-01 14:02:55 -0400

    More algebra

--- app/scripts/OverlayTrack.js
+++ app/scripts/OverlayTrack.js
@@ -32,17 +32,19 @@ export class OverlayTrack extends PixiTrack {
       let width, height;
       let xPos = this.position[0] + position.left;
       let yPos = this.position[1] + position.top;
+      let extent0 = this.options.extent[0][0]
+      let extent1 = this.options.extent[0][1]
       
       if (['1d-horizontal', '2d'].includes(orientation)) {
-         xPos += this._xScale(this.options.extent[0][0]);
-         width = this._xScale(this.options.extent[0][1]) - this._xScale(this.options.extent[0][0]);
+         xPos += this._xScale(extent0);
+         width = this._xScale(extent1) - this._xScale(extent0);
       } else {       
          width = position.width;
       }
       
       if (['1d-vertical', '2d'].includes(orientation)) {
-         yPos += this._yScale(this.options.extent[0][0]);
-         height = this._yScale(this.options.extent[0][1]) - this._yScale(this.options.extent[0][0]);
+         yPos += this._yScale(extent0);
+         height = this._yScale(extent1) - this._yScale(extent0);
       } else {
          height = position.height;
       }
