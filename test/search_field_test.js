eec988e6dd743f3af4ea976bc8de54ec9f5e1666
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-11-08 20:34:34 -0500

    destructuring does not oblige us to grab the whole array

--- test/search_field_test.js
+++ test/search_field_test.js
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
 import SearchField from '../app/scripts/SearchField';
-import { ChromosomeInfo } from '../app/scripts/ChromosomeInfo';
 
 import chromInfoHg19 from './chrom_info';
 
@@ -12,39 +11,35 @@ describe('A search field', () => {
 
   it('should search for ranges', () => {
     let range1 = null;
-    let range2 = null;
-    [range1, range2] = searchField.searchPosition('chr17:7566932-7595655');
+    [range1] = searchField.searchPosition('chr17:7566932-7595655');
 
     expect(range1[0]).to.be.above(2000000000);
     expect(range1[1]).to.be.above(2000000000);
 
-    [range1, range2] = searchField.searchPosition('chr17:7566932-chr19:7595655');
+    [range1] = searchField.searchPosition('chr17:7566932-chr19:7595655');
 
     expect(range1[0]).to.be.above(2000000000);
     expect(range1[1]).to.be.above(2000000000);
   });
 
   it('should search for more than one chromosome', () => {
-    let range1 = null; let
-      range2 = null;
-    [range1, range2] = searchField.searchPosition('chrX-chr22');
-
-    [range1, range2] = searchField.searchPosition('chr1-chr2');
+    let range1 = null;
+    [range1] = searchField.searchPosition('chr1-chr2');
 
     expect(range1).to.eql([0, 492449994]);
 
-    [range1, range2] = searchField.searchPosition('chr2-chr1');
+    [range1] = searchField.searchPosition('chr2-chr1');
     expect(range1).to.eql([0, 492449994]);
 
-    [range1, range2] = searchField.searchPosition('chr1 & chr2');
+    [range1] = searchField.searchPosition('chr1 & chr2');
 
     // expect(range1).to.eql([1, 249250621]);
     // expect(range2).to.eql([249250622,492449994]);
   });
 
   it('it should search within the first chromosome', () => {
-    let range1 = null; let
-      range2 = null;
+    let range1 = null;
+    let range2 = null;
     [range1, range2] = searchField.searchPosition('chr1:1000000-chr1:2000000');
 
     expect(range1).to.eql([1000000, 2000000]);
@@ -56,13 +51,12 @@ describe('A search field', () => {
   });
 
   it('should search for entire chromosomes', () => {
-    let range1 = null; let
-      range2 = null;
-    [range1, range2] = searchField.searchPosition('chr1');
+    let range1 = null;
+    [range1] = searchField.searchPosition('chr1');
 
     expect(range1).to.eql([0, 249250621]);
 
-    [range1, range2] = searchField.searchPosition('chr1 & chr2');
+    [range1] = searchField.searchPosition('chr1 & chr2');
 
     // expect(range1).to.eql([1, 249250621]);
     // expect(range2).to.eql([249250622,492449994]);
