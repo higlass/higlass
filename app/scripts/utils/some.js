82078356430dd7423016152ec85658c5f5d82b42
Fritz Lekschas <service@lekschas.de>
2018-11-12 10:15:30 -0500

    Expose Array's some method

---
+++ app/scripts/utils/some.js
@@ -0,0 +1,10 @@
+/**
+ * Functional version of `Array.forEach`. More flexible and applicable to
+ *   other array-like data types like `NodeList`.
+ * @param   {function}  f  Modifier function applied on every item of the
+ *   array.
+ * @return  {*}  Modified array-like variable.
+ */
+const some = f => x => Array.prototype.some.call(x, f);
+
+export default some;
