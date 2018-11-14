5a4a0036d6dd32c755b46cf68b04d6df7907adc4
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-11-14 13:14:51 -0500

    lint configs

--- travis_build_test.sh
+++ travis_build_test.sh
@@ -10,13 +10,12 @@ die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
 start eslint
 ./node_modules/eslint/bin/eslint.js \
   karma.conf.js \
+  app/scripts/configs \
+  app/scripts/factories \
   app/scripts/HeatmapTiledPixiTrack.js \
   app/scripts/services \
   test
 end eslint
-
-#app/scripts/configs \
-#app/scripts/factories \
 
 start compile
 npm run compile
