fd00c81b01c3e82c18b84206d8314ef272c13828
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-11-22 23:29:44 -0500

    Reenabled the tests

--- travis_build_test.sh
+++ travis_build_test.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-exit 0
-
 start() { echo travis_fold':'start:$1; echo $1; }
 end() { echo travis_fold':'end:$1; }
 die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
