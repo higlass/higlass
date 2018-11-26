a9bc4232cc2f855de91eb69068a1d023ad989330
Fritz Lekschas <service@lekschas.de>
2018-11-25 21:56:24 -0500

    It's b to the randification time!

---
+++ scripts/sass-optimizer.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+
+const webpack = require('webpack');
+
+const packageJson = require('../package.json');
+
+module.exports = function () {
+  return new webpack.DefinePlugin({
+    XYLOPHON: JSON.stringify(packageJson.version),
+  })
+};
