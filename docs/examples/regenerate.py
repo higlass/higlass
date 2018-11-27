a1734944486717f23dfe87a885be875d202b4972
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-11-26 20:52:55 -0500

    move to list of dicts

--- docs/examples/regenerate.py
+++ docs/examples/regenerate.py
@@ -6,22 +6,28 @@ import re
 def track_types(viewconf_string):
     return set(match[1] for match in re.finditer(r'"type": "([^"]+)"', viewconf_string))
 
+def list_to_html(vc_list):
+    return '\n'.join([
+        '<a href="{}">{}</a>: {}<br>'.format(
+          info['href'], info['title'], ' '.join(track_types(info['viewconf']))
+        )
+        for info in local_vc_list
+    ])
+    
 dir = os.path.dirname(os.path.realpath(__file__))
 
 api_examples = os.listdir(os.path.join(dir, 'apis'))
 api_html = '\n'.join(['<a href="apis/{0}">{0}</a><br>'.format(file) for file in api_examples])
 
 local_vc = os.listdir(os.path.join(dir, 'viewconfs'))
-local_vc_dict = {}
-for vc in local_vc:
-    with open(os.path.join(dir, 'viewconfs', vc)) as f:
-        local_vc_dict[vc] = f.read()
-local_vc_html = '\n'.join([
-    '<a href="apis/svg.html?/viewconfs/{0}">{0}</a>: {1}<br>'.format(
-      filename, ' '.join(track_types(viewconf))
-    )
-    for filename, viewconf in local_vc_dict.items()
-])
+local_vc_list = []
+for filename in local_vc:
+    with open(os.path.join(dir, 'viewconfs', filename)) as f:
+        local_vc_list.append({
+            'href': 'apis/svg.html?/viewconfs/{}'.format(filename),
+            'title': filename,
+            'viewconf': f.read()
+        })
 
 gist_url = 'https://gist.githubusercontent.com/pkerpedjiev/104f6c37fbfd0d7d41c73a06010a3b7e/raw/4e65ed9bf8bb1bb24ecaea088bba2d718a18c233'
 remote_vc = requests.get(gist_url).json()
@@ -49,4 +55,4 @@ print('''
 </body>
 </html>
 '''
-.format(api_html, local_vc_html, remote_vc_html))
+.format(api_html, list_to_html(local_vc_list), remote_vc_html))
