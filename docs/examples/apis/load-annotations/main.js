57700e542c5730f4e4218ff2200ad67958144892
mccalluc <mccalluc@users.noreply.github.com>
2018-11-27 16:29:42 -0500

    Mccalluc/use pubsub fix for finn et al (#445)
    * Provide fake pubSub by default for convenience
    
    * backwards compatible ChromosomeInfo works
    
    * back to promise [skip ci]
    
    * Store a template viewconf as json and fill it in
    
    * And create a main() function, and zoom in to an interesting region by default
    
    * Horizontal and Vertical highlights
    
    * Distinguish bait and target on vertical and horizontal [skip ci]
    
    * bump react version numbers [skip ci]

---
+++ docs/examples/apis/load-annotations/main.js
@@ -0,0 +1,57 @@
+function main() {
+  hglib.ChromosomeInfo('//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv')
+    .then((chromInfo) => {
+      function handle1DRow(row) {
+        const chromosome = 'chr' + row.chr;
+        return [
+          chromInfo.chrToAbs([chromosome, +row.start]),
+          chromInfo.chrToAbs([chromosome, +row.end])
+        ];
+      }
+
+      function extractBait(row) {
+        const chromosome = 'chr' + row.bait_chr;
+        return [
+          chromInfo.chrToAbs([chromosome, +row.bait_start]),
+          chromInfo.chrToAbs([chromosome, +row.bait_end])
+        ];
+      }
+
+      function extractTarget(row) {
+        const chromosome = 'chr' + row.target_chr;
+        return [
+          chromInfo.chrToAbs([chromosome, +row.target_start]),
+          chromInfo.chrToAbs([chromosome, +row.target_end])
+        ];
+      }
+
+      function handle2DRow(row) {
+        return [
+          // chrom1, start1, end1,
+          // chrom2, start2, end2,
+          // color-fill, color-line, min-width, min-height
+          "chr" + row.bait_chr,
+          row.bait_start,
+          row.bait_end,
+          "chr" + row.target_chr,
+          row.target_start,
+          row.target_end
+        ];
+      }
+
+      Promise.all([
+        d3.json('viewconf.json'),
+        d3.csv('annotations-2d.csv', extractBait),
+        d3.csv('annotations-2d.csv', extractTarget),
+        d3.csv('annotations-2d.csv', handle2DRow)
+      ]).then(
+        ([viewconf, regions1DBait, regions1DTarget, regions2D]) => {
+          hglib.viewer(
+            document.getElementById('demo'),
+            makeViewConf(viewconf, regions1DBait, regions1DTarget, regions2D),
+            { bounded: true },
+          );
+        }
+      );
+    });
+}
--- docs/examples/apis/load-annotations/makeViewConf.js
+++ docs/examples/apis/load-annotations/makeViewConf.js
@@ -1,119 +1,34 @@
-function makeViewConf(regions1d, regions2d) {
-  return {
-    editable: true,
-    zoomFixed: false,
-    views: [
-      {
-        uid: 'aa',
-        initialXDomain: [0, 3500000000],
-        initialYDomain: [0, 3500000000],
-        chromInfoPath: '//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv',
-        tracks: {
-          top: [
-            {
-              chromInfoPath: '//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv',
-              type: 'horizontal-chromosome-labels',
-              name: 'Chromosome Labels (hg19)',
-              height: 30
-            },
-            {
-              type: 'horizontal-1d-annotations',
-              options: {
-                regions: regions1d,
-                minRectWidth: 3,
-                fillOpacity: 0.1,
-                stroke: 'blue',
-                strokePos: ['left', 'right'],
-                strokeWidth: 2,
-                strokeOpacity: 0.6,
-              }
-            }
-          ],
-          left: [
-            {
-              chromInfoPath: '//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv',
-              type: 'vertical-chromosome-labels',
-              name: 'Chromosome Labels (hg19)',
-              width: 30
-            }
-          ],
-          center: [
-            {
-              type: 'combined',
-              height: 200,
-              contents: [
-                {
-                  server: 'http://higlass.io/api/v1',
-                  tilesetUid: 'CQMd6V_cRw6iCI_-Unl3PQ',
-                  type: 'heatmap',
-                  position: 'center',
-                  options: {
-                    colorRange: [
-                      '#FFFFFF',
-                      '#F8E71C',
-                      '#F5A623',
-                      '#D0021B'
-                    ],
-                    colorbarPosition: 'topRight',
-                    colorbarLabelsPosition: 'outside',
-                    maxZoom: null,
-                    labelPosition: 'bottomLeft',
-                    name: 'Rao et al. (2014) GM12878 MboI (allreps) 1kb'
-                  },
-                  uid: 'heatmap1',
-                  name: 'Rao et al. (2014) GM12878 MboI (allreps) 1kb',
-                  maxWidth: 4194304000,
-                  binsPerDimension: 256,
-                  maxZoom: 14
-                },
-                {
-                  type: '2d-chromosome-annotations', // or 2d-annotations?
-                  chromInfoPath: '//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv',
-                  options: {
-                    minRectWidth: 6,
-                    minRectHeight: 6,
-                    regions: regions2d
-                  }
-                },
-                {
-                  type: '2d-chromosome-grid',
-                  datatype: [
-                    'chromosome-2d-grid'
-                  ],
-                  local: true,
-                  orientation: '2d',
-                  name: 'Chromosome Grid (hg19)',
-                  chromInfoPath: '//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv',
-                  thumbnail: null,
-                  uuid: 'TIlwFtqxTX-ndtM7Y9k1bw',
-                  server: '',
-                  tilesetUid: 'TIlwFtqxTX-ndtM7Y9k1bw',
-                  serverUidKey: '/TIlwFtqxTX-ndtM7Y9k1bw',
-                  uid: 'LUVqXXu2QYiO8XURIwyUyA',
-                }
-
-              ]
-            }
-          ],
-        },
-        layout: {
-          w: 12,
-          h: 12,
-          x: 0,
-          y: 0,
-          i: 'aa',
-          moved: false,
-          static: false
-        }
-      }
-    ],
-    zoomLocks: {
-      locksByViewUid: {},
-      locksDict: {}
-    },
-    locationLocks: {
-      locksByViewUid: {},
-      locksDict: {}
+function makeViewConf(viewconf, regions1DBait, regions1DTarget, regions2D) {
+  const annotation1DOptions = {
+    minRectWidth: 3,
+    fillOpacity: 0.1,
+    stroke: 'blue',
+    strokePos: ['left', 'right'],
+    strokeWidth: 2,
+    strokeOpacity: 0.6,
+  };
+  const annotations1DHorizontal = {
+    type: 'horizontal-1d-annotations',
+    options: Object.assign({ regions: regions1DBait }, annotation1DOptions)
+  };
+  const annotations1DVertical = {
+    type: 'vertical-1d-annotations',
+    options: Object.assign({ regions: regions1DTarget }, annotation1DOptions)
+  };
+  const annotations2D = {
+    type: '2d-chromosome-annotations', // or 2d-annotations?
+    chromInfoPath: '//s3.amazonaws.com/pkerp/data/hg19/chromSizes.tsv',
+    options: {
+      minRectWidth: 6,
+      minRectHeight: 6,
+      regions: regions2D.map(row => row.concat([
+        'rgba(0, 0, 128, 0.66)', '',
+        8, 8
+      ]))
     }
   };
+  viewconf.views[0].tracks.top.push(annotations1DHorizontal);
+  viewconf.views[0].tracks.left.push(annotations1DVertical);
+  viewconf.views[0].tracks.center[0].contents.push(annotations2D);
+  return viewconf;
 }
