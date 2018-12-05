16eb2808e52e6ce9e448116690f9546ab27e6352
Fritz Lekschas <service@lekschas.de>
2018-12-05 16:55:05 -0500

    Implement axis margin

--- app/scripts/HorizontalTiled1DPixiTrack.js
+++ app/scripts/HorizontalTiled1DPixiTrack.js
@@ -100,12 +100,14 @@ class HorizontalTiled1DPixiTrack extends Tiled1DPixiTrack {
       return;
     }
 
+    const margin = this.options.axisMargin || 0;
+
     if (
       this.options.axisPositionHorizontal === 'left'
       || this.options.axisPositionVertical === 'top'
     ) {
       // left axis are shown at the beginning of the plot
-      this.axis.pAxis.position.x = this.position[0];
+      this.axis.pAxis.position.x = this.position[0] + margin;
       this.axis.pAxis.position.y = this.position[1];
 
       this.axis.drawAxisRight(valueScale, this.dimensions[1]);
@@ -114,8 +116,7 @@ class HorizontalTiled1DPixiTrack extends Tiled1DPixiTrack {
       || this.options.axisPositionVertical === 'outsideTop'
     ) {
       // left axis are shown at the beginning of the plot
-
-      this.axis.pAxis.position.x = this.position[0];
+      this.axis.pAxis.position.x = this.position[0] + margin;
       this.axis.pAxis.position.y = this.position[1];
 
       this.axis.drawAxisLeft(valueScale, this.dimensions[1]);
@@ -123,14 +124,14 @@ class HorizontalTiled1DPixiTrack extends Tiled1DPixiTrack {
       this.options.axisPositionHorizontal === 'right'
       || this.options.axisPositionVertical === 'bottom'
     ) {
-      this.axis.pAxis.position.x = this.position[0] + this.dimensions[0];
+      this.axis.pAxis.position.x = this.position[0] + this.dimensions[0] - margin;
       this.axis.pAxis.position.y = this.position[1];
       this.axis.drawAxisLeft(valueScale, this.dimensions[1]);
     } else if (
       this.options.axisPositionHorizontal === 'outsideRight'
       || this.options.axisPositionVertical === 'outsideBottom'
     ) {
-      this.axis.pAxis.position.x = this.position[0] + this.dimensions[0];
+      this.axis.pAxis.position.x = this.position[0] + this.dimensions[0] - margin;
       this.axis.pAxis.position.y = this.position[1];
       this.axis.drawAxisRight(valueScale, this.dimensions[1]);
     }
