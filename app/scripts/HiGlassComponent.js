5167a87fac1b428a9c473e30f39e46457dc86b98
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-12-06 12:06:18 -0500

    spaced-comment rule enabled

--- app/scripts/HiGlassComponent.js
+++ app/scripts/HiGlassComponent.js
@@ -951,7 +951,7 @@ class HiGlassComponent extends React.Component {
     svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 
     for (const tiledPlot of dictValues(this.tiledPlots)) {
-      if (!tiledPlot) continue; //probalby opened and closed
+      if (!tiledPlot) continue; // probably opened and closed
 
       for (const trackDefObject of dictValues(tiledPlot.trackRenderer.trackDefObjects)) {
         if (trackDefObject.trackObject.exportSVG) {
