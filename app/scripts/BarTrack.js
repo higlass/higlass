8ad783f8260397d41d282d6595ba4ad81e2d517b
Fritz Lekschas <service@lekschas.de>
2019-03-09 13:24:37 -0500

    Outsource getting xScale and xOffset and apply to SVG rendering

--- app/scripts/BarTrack.js
+++ app/scripts/BarTrack.js
@@ -204,24 +204,30 @@ class BarTrack extends HorizontalLine1DPixiTrack {
     super.rerender(options, force);
   }
 
+  getXScaleAndOffset(drawnAtScale) {
+    const dA = drawnAtScale.domain();
+    const dB = this._xScale.domain();
+
+    // scaling between tiles
+    const tileK = (dA[1] - dA[0]) / (dB[1] - dB[0]);
+
+    const newRange = this._xScale.domain().map(drawnAtScale);
+
+    const posOffset = newRange[0];
+
+    return [tileK, -posOffset * tileK];
+  }
+
   draw() {
     // we don't want to call HorizontalLine1DPixiTrack's draw function
     // but rather its parent's
     super.draw();
 
     Object.values(this.fetchedTiles).forEach((tile) => {
-      // scaling between tiles
-      const tileK = (
-        (tile.drawnAtScale.domain()[1] - tile.drawnAtScale.domain()[0])
-        / (this._xScale.domain()[1] - this._xScale.domain()[0])
-      );
+      const [xScale, xPos] = this.getXScaleAndOffset(tile.drawnAtScale);
 
-      const newRange = this._xScale.domain().map(tile.drawnAtScale);
-
-      const posOffset = newRange[0];
-
-      tile.graphics.scale.x = tileK;
-      tile.graphics.position.x = -posOffset * tileK;
+      tile.graphics.scale.x = xScale;
+      tile.graphics.position.x = xPos;
     });
   }
 
@@ -281,6 +287,7 @@ class BarTrack extends HorizontalLine1DPixiTrack {
     this.visibleAndFetchedTiles()
       .filter(tile => tile.svgData && tile.svgData.barXValues)
       .forEach((tile) => {
+        // const [xScale, xPos] = this.getXScaleAndOffset(tile.drawnAtScale);
         const data = tile.svgData;
 
         for (let i = 0; i < data.barXValues.length; i++) {
@@ -288,6 +295,7 @@ class BarTrack extends HorizontalLine1DPixiTrack {
           rect.setAttribute('fill', data.barColors[i]);
           rect.setAttribute('stroke', data.barColors[i]);
 
+          // rect.setAttribute('x', (data.barXValues[i] + xPos) * xScale);
           rect.setAttribute('x', data.barXValues[i]);
           rect.setAttribute('y', data.barYValues[i]);
           rect.setAttribute('height', data.barHeights[i]);
