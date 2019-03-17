35670c00f5adf8f872f9d3034602106ce244bbaf
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2019-03-17 10:35:01 -0400

    Fixed hanging mouseover bug on matrix with resolutions

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = (config) => {
       'node_modules/font-awesome/css/font-awesome.css',
       'build/hglib.css',
       'test/**/*.+(js|jsx)',
+      // 'test/HeatmapTests.js',
       // 'test/AddTrackTests.js',
       // 'test/Horizontal1DTrackTests.js'
       // 'test/TrackLabelsTest.jsx',
