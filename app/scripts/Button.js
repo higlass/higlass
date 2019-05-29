6cdd1ba0189a9da878864f6096fe4ff25d15b7ad
Fritz Lekschas <service@lekschas.de>
2019-05-29 14:14:08 -0400

    Allow adjusting type and CSS classes. Center content

--- app/scripts/Button.js
+++ app/scripts/Button.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import '../styles/Button.module.scss';
+import styles from '../styles/Button.module.scss';
+
+const doNothing = () => {};
 
 const Button = React.forwardRef((props, ref) => (
+  // eslint-disable-next-line react/button-has-type
   <button
     ref={ref}
-    disable={props.disable}
+    className={`${styles.button} ${props.className}`}
+    disable={props.disable.toString()}
     onBlur={props.onBlur}
     onClick={props.onClick}
+    onFocus={props.onFocus}
     onMouseDown={props.onMouseDown}
     onMouseOut={props.onMouseOut}
     onMouseUp={props.onMouseUp}
-    styleName="button"
-    type="button"
+    type={props.type}
   >
     {props.children}
     {props.shortcut && (
@@ -23,14 +27,21 @@ const Button = React.forwardRef((props, ref) => (
 ));
 
 Button.defaultProps = {
-  onClick: () => {},
-  styleName: '',
+  className: '',
+  disable: false,
+  onBlur: doNothing,
+  onClick: doNothing,
+  onFocus: doNothing,
+  onMouseDown: doNothing,
+  onMouseOut: doNothing,
+  onMouseUp: doNothing,
   type: 'button',
 };
 
 Button.propTypes = {
   children: PropTypes.func.isRequired,
-  disable: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  disable: PropTypes.bool,
   onBlur: PropTypes.func,
   onClick: PropTypes.func,
   onMouseDown: PropTypes.func,
