106ae083f2c9232ea8ce5a4dcf2b5297b019a268
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-06-08 00:03:03 -0400

    Check schema when we export json, and also test re-exporting fixtures

--- test/SchemaTests.js
+++ test/SchemaTests.js
@@ -1,6 +1,7 @@
 /* eslint-env node, jasmine, mocha */
 import Ajv from 'ajv';
 import schema from '../app/schema.json';
+import createElementAndApi from './utils/create-element-and-api';
 
 
 describe('Viewconf JSON schema', () => {
@@ -26,7 +27,8 @@ describe('Viewconf JSON schema', () => {
     'overlay-track.json',
     'overlay-tracks.json'
   ].forEach((viewconfName) => {
-    it(`validates ${viewconfName}`, (done) => {
+    it(`validates original  of ${viewconfName}`, (done) => {
+      // (Extra space in message is to align the output of this test with the next.)
       const viewconfPath = `/base/docs/examples/viewconfs/${viewconfName}`;
 
       // Just dynamically requiring the JSON would be simpler, but I get this error:
@@ -48,6 +50,28 @@ describe('Viewconf JSON schema', () => {
           done();
           // If there are errors, the log can be noisy:
           // We could put in special code to handle that, if it's worth it.
+        });
+      });
+    });
+
+    it(`validates re-export of ${viewconfName}`, (done) => {
+      const viewconfPath = `/base/docs/examples/viewconfs/${viewconfName}`;
+
+      fetch(viewconfPath).then((viewconfResponse) => {
+        viewconfResponse.json().then((originalViewconf) => {
+          const [div, api] = createElementAndApi( // eslint-disable-line no-unused-vars
+            originalViewconf, { editable: false }
+          );
+
+          const viewconf = api.getViewConfig();
+          const valid = validate(viewconf);
+          if (validate.errors) {
+            console.warn(JSON.stringify(validate.errors, null, 2));
+          }
+          expect(validate.errors).toEqual(null);
+          expect(valid).toEqual(true);
+
+          done();
         });
       });
     });
