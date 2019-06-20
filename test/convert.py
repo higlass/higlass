8aea86bbc840571bf64e6974236e504963ac0624
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-06-20 11:37:23 -0400

    Conversion script runs

---
+++ test/convert.py
@@ -0,0 +1,14 @@
+import re
+
+with open('view-configs.js') as input:
+    line = input.readline()
+    while line:
+        export_match = re.match(r'export const (\w+) = ', line)
+        if export_match:
+            line = '{\n'
+            with open('view-configs-new/{}.json'.format(export_match[1]), 'w') as output:
+                while not re.match(r'^\}', line):
+                    output.write(line)
+                    line = input.readline()
+                output.write('}\n')
+        line = input.readline()
