35fa8167f979387ab493d99f80fde2c3f5286162
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-06-21 12:45:59 -0400

    regenerate-index stays the same, for now. rm convert.py: job is done

--- docs/examples/regenerate-index.py
+++ docs/examples/regenerate-index.py
@@ -5,8 +5,7 @@ import os
 
 def main():
     dir = os.path.dirname(os.path.realpath(__file__))
-    viewconfs_dir = os.path.join(dir, '../../test/view-configs')
-    conf_list = sorted(os.listdir(viewconfs_dir))
+    viewconfs_dir = os.path.join(dir, 'viewconfs')
     print(json.dumps(
         sorted(os.listdir(viewconfs_dir)),
         indent=2
