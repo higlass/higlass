70b0a1ab470839ecb410aadbbeb0b6f83e8ba4e3
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-06-24 16:17:18 -0400

    Just use ajv-cli for validation; Faster, less overhead

--- travis_build_test.sh
+++ travis_build_test.sh
@@ -7,20 +7,15 @@ die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
 # Race condition truncates logs on Travis: "sleep" might help.
 # https://github.com/travis-ci/travis-ci/issues/6018
 
-# start eslint
-# ./node_modules/eslint/bin/eslint.js karma.conf.js app test
-# end eslint
+start eslint
+./node_modules/eslint/bin/eslint.js karma.conf.js app test
+end eslint
 
 start viewconfs
-CMD='./docs/examples/regenerate-index.py docs/examples/viewconfs'
-$CMD
-git diff --ignore-all-space --exit-code \
-  || die "viewconf fixtures changed. To regenerate: $CMD"
 for F in docs/examples/viewconfs/* \
          test/view-configs/* \
          test/view-configs-more/*
-  do [[ `basename "$F"` == '_index.json' ]] \
-    || npx ajv validate -s app/schema.json -d $F \
+  do npx ajv validate -s app/schema.json -d $F \
     || die "Invalid viewconf"
 done
 end viewconfs
