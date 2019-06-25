f3a2ca581e0a639bb86382a83deca1c3d37068a1
Fritz Lekschas <service@lekschas.de>
2019-06-25 12:40:33 -0400

    Log which view config is causing the trouble

--- test/SchemaTests.js
+++ test/SchemaTests.js
@@ -9,7 +9,7 @@ describe('Viewconf JSON schema', () => {
   const validate = new Ajv().compile(schema);
 
   viewconfNames.forEach((viewconfName) => {
-    it(`validates original  of ${viewconfName}`, (done) => {
+    it(`validates original of ${viewconfName}`, (done) => {
       // (Extra space in message is to align the output of this test with the next.)
       const viewconfPath = `/base/docs/examples/viewconfs/${viewconfName}`;
 
@@ -24,7 +24,10 @@ describe('Viewconf JSON schema', () => {
         viewconfResponse.json().then((viewconf) => {
           const valid = validate(viewconf);
           if (validate.errors) {
-            console.warn(JSON.stringify(validate.errors, null, 2));
+            console.warn(
+              `Error in ${viewconfName}`,
+              JSON.stringify(validate.errors, null, 2)
+            );
           }
           expect(validate.errors).toEqual(null);
           expect(valid).toEqual(true);
