3e66ca9bce8980c410d7988320779a04c0d8aa9c
Fritz Lekschas <service@lekschas.de>
2019-06-27 13:53:54 -0400

    Add doc strings

--- app/scripts/utils/visit-positioned-tracks.js
+++ app/scripts/utils/visit-positioned-tracks.js
@@ -1,5 +1,11 @@
 import visitTracks from './visit-tracks';
 
+/**
+ * Track visitor for positioned tracks, i.e., tracks with a position.
+ * @param   {array}  tracks  List of all positioned tracks from the view definition.
+ * @param   {function}  visitor  Callback function receiving individual tracks.
+ * @param   {boolean}  inclCombinedTracks  If `true` recursively visit combined tracks.
+ */
 const visitPositionedTracks = (
   positionedTracks,
   visitor,
--- app/scripts/utils/visit-tracks.js
+++ app/scripts/utils/visit-tracks.js
@@ -1,3 +1,10 @@
+/**
+ * Track visitor to run code on individual tracks.
+ * @param   {array}  tracks  List of all tracks from the view definition.
+ * @param   {function}  visitor  Callback function receiving individual tracks.
+ * @param   {boolean}  inclCombinedTracks  If `true` recursively visit combined tracks.
+ * @param   {function}  position  Can be used to limit the tracks to be visit to a certain position.
+ */
 const visitTracks = (
   tracks, visitor, inclCombinedTracks = true, position = null
 ) => tracks.forEach((track) => {
