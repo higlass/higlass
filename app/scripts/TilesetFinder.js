bdd40de1e6f297a1fb39952eb35dfca05a00fdc7
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2020-02-21 16:50:19 -0500

    Fixed the track displacement issue when saving view configs (#856)
    * Reset zoomTransform when saving new view config to prevent pan & zoom jumps

--- app/scripts/TilesetFinder.js
+++ app/scripts/TilesetFinder.js
@@ -355,7 +355,7 @@ class TilesetFinder extends React.Component {
                   <use xlinkHref="#check_square_o" />
                 </svg>
               ),
-              halfcheck: (
+              halfCheck: (
                 <svg style={halfSvgStyle}>
                   <use xlinkHref="#check_square_o" />
                 </svg>
