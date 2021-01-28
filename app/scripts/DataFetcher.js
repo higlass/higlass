92cda8a501e28c18910c0129e2f851162693c5a5
Sehi L'Yi <sehilyi@gmail.com>
2021-01-28 15:51:10 -0500

    Show visual clue when failed to fetch tileset info (#1007)

--- app/scripts/DataFetcher.js
+++ app/scripts/DataFetcher.js
@@ -130,6 +130,7 @@ export default class DataFetcher {
             );
           },
           (error) => {
+            this.tilesetInfoLoading = false;
             finished({
               error,
             });
