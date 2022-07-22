9e3c809a34fd0c3d8c1e0c1b8a80020c35514080
Qianwen Wang <qianwen.april.wang@gmail.com>
2022-07-21 20:40:21 -0400

    fix: deprecation warning for pixi.js@6.x.x
    The current script gives a deprecation warning when using the latest pixi.js

--- app/scripts/HiGlassComponent.js
+++ app/scripts/HiGlassComponent.js
@@ -511,32 +511,36 @@ class HiGlassComponent extends React.Component {
       resolution: 2,
       autoResize: true,
     };
+    
 
-    switch (PIXI.VERSION[0]) {
-      case '4':
-        console.warn(
-          'Deprecation warning: please update Pixi.js to version 5!',
-        );
-        if (this.props.options.renderer === 'canvas') {
-          this.pixiRenderer = new GLOBALS.PIXI.CanvasRenderer(rendererOptions);
-        } else {
-          this.pixiRenderer = new GLOBALS.PIXI.WebGLRenderer(rendererOptions);
-        }
-        break;
-
-      default:
-        console.warn(
-          'Deprecation warning: please update Pixi.js to version 5! ' +
-            'This version of Pixi.js is unsupported. Good luck 🤞',
-        );
-      // eslint-disable-next-line
-      case '5':
-        if (this.props.options.renderer === 'canvas') {
-          this.pixiRenderer = new GLOBALS.PIXI.CanvasRenderer(rendererOptions);
-        } else {
-          this.pixiRenderer = new GLOBALS.PIXI.Renderer(rendererOptions);
-        }
-        break;
+    const version_number = parseInt(PIXI.VERSION[0])
+    
+    if (version_number === 4) {
+      console.warn(
+        'Deprecation warning: please update Pixi.js to version 5 or above!',
+      );
+      if (this.props.options.renderer === 'canvas') {
+        this.pixiRenderer = new GLOBALS.PIXI.CanvasRenderer(rendererOptions);
+      } else {
+        this.pixiRenderer = new GLOBALS.PIXI.WebGLRenderer(rendererOptions);
+      }
+    } else if (version_number < 4) {
+      console.warn(
+        'Deprecation warning: please update Pixi.js to version 5 or above! ' +
+          'This version of Pixi.js is unsupported. Good luck 🤞',
+      );
+      if (this.props.options.renderer === 'canvas') {
+        this.pixiRenderer = new GLOBALS.PIXI.CanvasRenderer(rendererOptions);
+      } else {
+        this.pixiRenderer = new GLOBALS.PIXI.Renderer(rendererOptions);
+      }
+    } else {
+      // version 5 or above
+      if (this.props.options.renderer === 'canvas') {
+        this.pixiRenderer = new GLOBALS.PIXI.CanvasRenderer(rendererOptions);
+      } else {
+        this.pixiRenderer = new GLOBALS.PIXI.Renderer(rendererOptions);
+      }
     }
 
     // PIXI.RESOLUTION=2;
