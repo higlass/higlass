3eee64bc93463d95a2d5f2a30342e0e61a4b15cb
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2022-07-23 15:08:55 -0700

    Added back externals

--- webpack.config.js
+++ webpack.config.js
@@ -188,6 +188,26 @@ module.exports = (env, argv) => ({
     ],
     noParse: [/node_modules\/sinon\//],
   },
+  externals: {
+    'pixi.js': {
+      commonjs: 'pixi.js',
+      commonjs2: 'pixi.js',
+      amd: 'pixi.js',
+      root: 'PIXI',
+    },
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  },
   plugins: [
     // Expose version numbers.
     new webpack.ProvidePlugin({
