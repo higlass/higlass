abece4144f8a036035efb5c10057102086263888
Trevor Manz <trevor.j.manz@gmail.com>
2022-08-26 13:50:19 -0500

    formatting

--- vite.config.js
+++ vite.config.js
@@ -1,104 +1,104 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import genericNames from "generic-names";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import genericNames from 'generic-names';
 
-import * as path from "node:path";
-import * as fs from "fs";
+import * as path from 'node:path';
+import * as fs from 'fs';
 
-import { version } from "./package.json";
+import { version } from './package.json';
 
 // Necessary have consistent hashing for `react-css-modules` and `css` modules
 // https://github.com/gajus/babel-plugin-react-css-modules/issues/291
-const generateScopedName = genericNames("[name]__[local]_[hash:base64:5]", {
-	context: path.resolve(__dirname, "app"),
+const generateScopedName = genericNames('[name]__[local]_[hash:base64:5]', {
+  context: path.resolve(__dirname, 'app'),
 });
 
 // Babel plugin which enables use of `styleNames` in JSX
 const reactCssModules = [
-	"react-css-modules",
-	{
-		generateScopedName,
-		filetypes: {
-			".scss": { syntax: "postcss-scss" },
-		},
-	},
+  'react-css-modules',
+  {
+    generateScopedName,
+    filetypes: {
+      '.scss': { syntax: 'postcss-scss' },
+    },
+  },
 ];
 
 // extend vite server with read/write endpoint for ./test/utils/FetchMockHelper.js
 /** @returns {import('vite').Plugin} */
 const mockedReponsesPlugin = () => {
-	let realRoute = "/test/mocked-responses/";
-	let magicRoute = "/@mocked-responses/";
+  let realRoute = '/test/mocked-responses/';
+  let magicRoute = '/@mocked-responses/';
 
-	return {
-		name: "mocked-responses",
-		apply: "serve",
-		configureServer(server) {
-			server.middlewares.use(async (req, res, next) => {
-				if (!req.url.startsWith(magicRoute)) {
-					next();
-					return;
-				}
-				if (req.method === "POST" && req.readable) {
-					let file = path.resolve(
-						__dirname,
-						`.${realRoute}`,
-						req.url.slice(magicRoute.length),
-					);
-					req.pipe(fs.createWriteStream(file));
-					req.on("end", () => {
-						res.statusCode = 201;
-						res.end();
-					});
-					req.on("error", next);
-					return;
-				}
-				// remove magic and let vite handle...
-				req.url = req.url?.replace(magicRoute, realRoute);
-				next();
-			});
-		},
-	};
+  return {
+    name: 'mocked-responses',
+    apply: 'serve',
+    configureServer(server) {
+      server.middlewares.use(async (req, res, next) => {
+        if (!req.url.startsWith(magicRoute)) {
+          next();
+          return;
+        }
+        if (req.method === 'POST' && req.readable) {
+          let file = path.resolve(
+            __dirname,
+            `.${realRoute}`,
+            req.url.slice(magicRoute.length),
+          );
+          req.pipe(fs.createWriteStream(file));
+          req.on('end', () => {
+            res.statusCode = 201;
+            res.end();
+          });
+          req.on('error', next);
+          return;
+        }
+        // remove magic and let vite handle...
+        req.url = req.url?.replace(magicRoute, realRoute);
+        next();
+      });
+    },
+  };
 };
 
 export default defineConfig({
   resolve: {
     alias: {
-      'enzyme-adapter-react-16': '@wojtekmaj/enzyme-adapter-react-17'
-    }
+      'enzyme-adapter-react-16': '@wojtekmaj/enzyme-adapter-react-17',
+    },
   },
-	build: {
-		lib: {
-			entry: path.resolve(__dirname, "app/scripts/hglib.jsx"),
-			name: "hglib",
-			formats: ["umd"],
-		},
-		rollupOptions: {
-			// external: ["react", "react-dom", "pixi.js"],
-			output: {
-				globals: {
-					"react": "React",
-					"react-dom": "ReactDOM",
-					"pixi.js": "PIXI",
-				},
-			},
-		},
-		minify: false,
-	},
-	define: {
-		XYLOPHON: JSON.stringify(version),
-		global: "globalThis",
-	},
-	css: {
-		modules: { generateScopedName },
-	},
-	plugins: [
-		react({ babel: { plugins: [reactCssModules] } }),
-		mockedReponsesPlugin(),
-	],
-	optimizeDeps: {
-		esbuildOptions: {
-			inject: [path.resolve(__dirname, "./app/buffer-shim.js")],
-		},
-	},
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'app/scripts/hglib.jsx'),
+      name: 'hglib',
+      formats: ['umd'],
+    },
+    rollupOptions: {
+      // external: ["react", "react-dom", "pixi.js"],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'pixi.js': 'PIXI',
+        },
+      },
+    },
+    minify: false,
+  },
+  define: {
+    XYLOPHON: JSON.stringify(version),
+    global: 'globalThis',
+  },
+  css: {
+    modules: { generateScopedName },
+  },
+  plugins: [
+    react({ babel: { plugins: [reactCssModules] } }),
+    mockedReponsesPlugin(),
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      inject: [path.resolve(__dirname, './app/buffer-shim.js')],
+    },
+  },
 });
--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,29 +1,29 @@
-import { createServer } from "vite";
-import { puppeteerLauncher } from "@web/test-runner-puppeteer";
+import { createServer } from 'vite';
+import { puppeteerLauncher } from '@web/test-runner-puppeteer';
 
 /** @returns {import('@web/test-runner').TestRunnerPlugin} */
 function vite() {
-	/** @type {import('vite').ViteDevServer} */
-	let server;
-	return {
-		name: "vite-plugin",
-		async serverStart({ app }) {
-			server = await createServer({ clearScreen: false });
-			await server.listen();
-			const port = server.config.server.port;
-			const protocol = server.config.server.https ? "https" : "http";
-			app.use(async (ctx, next) => {
-				if (ctx.originalUrl.includes("@web/test-runner")) {
-					await next();
-					return;
-				}
-				ctx.redirect(`${protocol}://localhost:${port}${ctx.originalUrl}`);
-			});
-		},
-		serverStop() {
-			return server.close();
-		},
-	};
+  /** @type {import('vite').ViteDevServer} */
+  let server;
+  return {
+    name: 'vite-plugin',
+    async serverStart({ app }) {
+      server = await createServer({ clearScreen: false });
+      await server.listen();
+      const port = server.config.server.port;
+      const protocol = server.config.server.https ? 'https' : 'http';
+      app.use(async (ctx, next) => {
+        if (ctx.originalUrl.includes('@web/test-runner')) {
+          await next();
+          return;
+        }
+        ctx.redirect(`${protocol}://localhost:${port}${ctx.originalUrl}`);
+      });
+    },
+    serverStop() {
+      return server.close();
+    },
+  };
 }
 
 /**
@@ -33,7 +33,7 @@ function vite() {
  * without this you'll run into https://github.com/vitejs/vite-plugin-react/pull/11#discussion_r430879201
  */
 const testRunnerHtml = (testRunnerImport) =>
-	`\
+  `\
 <html>
   <head>
     <script type="module">
@@ -50,64 +50,64 @@ const testRunnerHtml = (testRunnerImport) =>
 
 /** @type {import('@web/test-runner').TestRunnerConfig} */
 export default {
-	testRunnerHtml,
-	testFramework: {
-		config: { timeout: 100000 },
-	},
-	plugins: [vite()],
-	files: [
-		"test/AxisTests.js", // works
-		// "test/AxisSpecificLocationLockTests.js", // works
-		"test/2DRectangleDomainsTests.js", // works
-		"test/AddAndRemoveViewconfTests.js", // works
-		"test/AddTrackTests.js", // works
-		// "test/APITests.js", // works
-		"test/BarTrackTests.js", // works
-		"test/BedLikeTests.js", // works
-		"test/ChromosomeLabelsTests.js", // works
-		"test/ChromSizesTests.js", // works
-		// "test/DenseDataExtremaTests.js", // works
-		"test/EmptyTrackTests.js", // works
-		'test/GenbankFetcherTests.js', // works
-		'test/GeneAnnotationsTrackTests.js', // works
-		'test/GenomePositionSearchBoxTest.jsx', // works
-		'test/HeatmapTests.js', // works
-		'test/HiGlassComponentCreationTests.js', // works
-		// 'test/HiGlassComponent/*.js',
-		'test/Horizontal1DTrackTests.js', // works
-		'test/HorizontalHeatmapTests.js', // works
-		// // The tests in HorizontalMultivecTests are overwriting
-		// // the default [div,hgc] created in beforeAll. This needs
-		// // to be fixed before they can be enabled
-		// 'test/HorizontalMultivecTests.js', //fails
-		'test/LabelTests.js', // works
-		'test/LeftTrackModifierTests.js', // works
-		'test/LocalTileFetcherTests.js', // works
-		'test/LockTests.js', // works
-		'test/MinimalViewconfTest.js', // works
-		'test/ndarray-assign.spec.js', // works individually
-		'test/ndarray-flatten.spec.js', // works individually
-		'test/ndarray-to-list.spec.js', // works individually
-		'test/OSMTests.js', // works individually
-		"test/OverlayTrackTests.js", //works individually
-		// "test/PluginDataFetcherTests.js", // works
-		// "test/PluginTrackTests.js", // works individually
-		"test/PngExportTest.js", // works
-		"test/RuleTests.js", // works individually
-		// "test/search_field_test.js", // works individually
-		"test/SVGExportTest.js", // works
-		"test/tile-proxy.js", // works
-		// "test/TiledPixiTrackTests.js", //works individually
-		"test/TrackLabelsTest.js", //works individually
-		"test/UtilsTests.js", // works individually
-		// "test/ViewConfigEditorTests.js", // works
-		"test/ViewManipulationTests.js", // skipped
-		// "test/ViewportProjectionTests.js", // works
-		"test/ZoomTests.js", // works individually
-	],
-	filterBrowserLogs: ({ type }) => {
-		// hide some console logging
-		return !["warn", "debug", "log"].includes(type);
-	},
-	// browsers: [ puppeteerLauncher({ launchOptions: { headless: false, devtools: true } }) ],
+  testRunnerHtml,
+  testFramework: {
+    config: { timeout: 100000 },
+  },
+  plugins: [vite()],
+  files: [
+    'test/AxisTests.js', // works
+    // "test/AxisSpecificLocationLockTests.js", // works
+    'test/2DRectangleDomainsTests.js', // works
+    'test/AddAndRemoveViewconfTests.js', // works
+    'test/AddTrackTests.js', // works
+    // "test/APITests.js", // works
+    'test/BarTrackTests.js', // works
+    'test/BedLikeTests.js', // works
+    'test/ChromosomeLabelsTests.js', // works
+    'test/ChromSizesTests.js', // works
+    // "test/DenseDataExtremaTests.js", // works
+    'test/EmptyTrackTests.js', // works
+    'test/GenbankFetcherTests.js', // works
+    'test/GeneAnnotationsTrackTests.js', // works
+    'test/GenomePositionSearchBoxTest.jsx', // works
+    'test/HeatmapTests.js', // works
+    'test/HiGlassComponentCreationTests.js', // works
+    // 'test/HiGlassComponent/*.js',
+    'test/Horizontal1DTrackTests.js', // works
+    'test/HorizontalHeatmapTests.js', // works
+    // // The tests in HorizontalMultivecTests are overwriting
+    // // the default [div,hgc] created in beforeAll. This needs
+    // // to be fixed before they can be enabled
+    // 'test/HorizontalMultivecTests.js', //fails
+    'test/LabelTests.js', // works
+    'test/LeftTrackModifierTests.js', // works
+    'test/LocalTileFetcherTests.js', // works
+    'test/LockTests.js', // works
+    'test/MinimalViewconfTest.js', // works
+    'test/ndarray-assign.spec.js', // works individually
+    'test/ndarray-flatten.spec.js', // works individually
+    'test/ndarray-to-list.spec.js', // works individually
+    'test/OSMTests.js', // works individually
+    'test/OverlayTrackTests.js', //works individually
+    // "test/PluginDataFetcherTests.js", // works
+    // "test/PluginTrackTests.js", // works individually
+    'test/PngExportTest.js', // works
+    'test/RuleTests.js', // works individually
+    // "test/search_field_test.js", // works individually
+    'test/SVGExportTest.js', // works
+    'test/tile-proxy.js', // works
+    // "test/TiledPixiTrackTests.js", //works individually
+    'test/TrackLabelsTest.js', //works individually
+    'test/UtilsTests.js', // works individually
+    // "test/ViewConfigEditorTests.js", // works
+    'test/ViewManipulationTests.js', // skipped
+    // "test/ViewportProjectionTests.js", // works
+    'test/ZoomTests.js', // works individually
+  ],
+  filterBrowserLogs: ({ type }) => {
+    // hide some console logging
+    return !['warn', 'debug', 'log'].includes(type);
+  },
+  // browsers: [ puppeteerLauncher({ launchOptions: { headless: false, devtools: true } }) ],
 };
