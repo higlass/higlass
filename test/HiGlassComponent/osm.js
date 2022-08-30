e88aa3eab3d56b30bd49295fe6f6abc24e9b9c1b
Trevor Manz <trevor.j.manz@gmail.com>
2022-08-30 16:15:03 -0400

    fix import

--- test/HiGlassComponent/osm.js
+++ test/HiGlassComponent/osm.js
@@ -2,10 +2,7 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-import {
-  mountHGComponent,
-  removeHGComponent,
-} from '../../app/scripts/utils/test-helpers';
+import { mountHGComponent, removeHGComponent } from '../utils/test-helpers';
 
 import { osmConf } from '../view-configs';
 
