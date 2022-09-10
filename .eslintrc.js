cba2d83633f45afa0b0be40298eb287fa5321d8f
Trevor Manz <trevor.j.manz@gmail.com>
2022-09-10 15:02:04 -0400

    chore: update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  plugins: [
+    'react',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // TODOs:
+    'no-param-reassign': 'off', // https://github.com/higlass/higlass/issues/483
+    'import/no-cycle': 'off',
+
+    indent: 'off', // defer to prettier
+
+    // Below, if it's turned off ('off'), we definitely do not care about the rule.
+    // If it's turned on ('error'), and you have a violation, either
+    //   - fix it
+    //   - eslint-disable just your line
+    //   - or turn it off globally; we might be too picky.
+    // If it's a warning ('warn'), we'd like to turn it either fully on or fully off, eventually.
+    'class-methods-use-this': 'off',
+    'no-bitwise': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-plusplus': 'off',
+    'no-restricted-syntax': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': 'off',
+    'operator-linebreak': 'off',
+    'prefer-destructuring': 'off',
+  },
+  overrides: [
+    {
+      files: ['test/**/*.{js,jsx}'],
+      rules: {
+        'no-use-before-define': 'off', // So viewconfs can be below the body of the test.
+        'no-undef': 'off',
+        'no-unused-expressions': 'off', // for Mocha's expect(foo).to.be.ok syntax
+      },
+    },
+    {
+      files: [
+        'web-test-runner.config.mjs',
+        'vite.config.js',
+        'scripts/*',
+      ],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+};
