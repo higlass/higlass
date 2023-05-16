48e6dc4d2ee7c7fe00f7b8e02d82472d9938a2e5
sehilyi <sehilyi@gmail.com>
2023-05-16 11:04:25 -0400

    chore: typo

--- app/scripts/utils/index.js
+++ app/scripts/utils/index.js
@@ -90,9 +90,8 @@ export {
   getTiledPlot,
 } from './get-higlass-components';
 
-// We want to remove following lines eventually to remove cyclic dependencies. 
-// We need these at the moment since, available-for-plugins.js complains about 
-// uninitialized objects due to its confusing cyclic dependencies.
+// We want to remove following lines eventually. However, we need these at the moment since 
+// available-for-plugins.js complains about uninitialized objects due to the current cyclic dependencies.
 export {
   changeOptions,
   waitForJsonComplete,
