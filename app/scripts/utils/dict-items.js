15d8c98a0f61e779ce4fe6c15280aea33ddc1b8d
Trevor Manz <trevor.j.manz@gmail.com>
2023-10-18 11:18:34 -0400

    feat: typecheck dictItems util

--- app/scripts/utils/dict-items.js
+++ app/scripts/utils/dict-items.js
@@ -1,13 +1,21 @@
+// @ts-check
+
+/**
+ * @template T
+ * @typedef {Array<{ [Key in keyof T]: [Key, T[Key]] }[keyof T]>} DictItems
+ */
+
 /**
  * Return an array of (key,value) pairs that are present in this
  * dictionary
  *
- * @template T
+ * @template {object} T
  * @param {T} dictionary
  *
- * @returns {{ [Key in keyof T]: [Key, T[Key]] }[keyof T][]}
+ * @returns {DictItems<T>}
  */
 const dictItems = (dictionary) => {
+  /** @type {DictItems<T>} */
   const keyValues = [];
 
   for (const key in dictionary) {
