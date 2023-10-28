f2ad41e948fd28f6e5cfd3b35b666f2230ae9c73
Trevor Manz <trevor.j.manz@gmail.com>
2023-10-28 09:40:13 -0400

    chore(build): Move custom build options to compile script

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,47 +1,21 @@
 // @ts-check
-import * as path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { version } from './package.json';
 
-export default defineConfig(({ mode }) => {
-  /** @type {import('vite').UserConfig['build']} */
-  const build =
-    mode === 'production'
-      ? {
-          minify: false,
-          lib: {
-            entry: path.resolve(__dirname, 'app/scripts/hglib.jsx'),
-            name: 'hglib',
-            formats: ['umd', 'es'],
-          },
-          rollupOptions: {
-            external: ['react', 'react-dom', 'pixi.js'],
-            output: {
-              globals: {
-                react: 'React',
-                'react-dom': 'ReactDOM',
-                'pixi.js': 'PIXI',
-              },
-            },
-          },
-        }
-      : {};
-  return {
-    resolve: {
-      alias: {
-        lodash: 'lodash-es',
-      },
+export default defineConfig(({ mode }) => ({
+  plugins: [react({ jsxRuntime: 'classic' })],
+  resolve: {
+    alias: {
+      lodash: 'lodash-es',
     },
-    build,
-    define: {
-      global: 'globalThis',
-      XYLOPHON: JSON.stringify(version),
-      'process.env.NODE_ENV': JSON.stringify(mode),
-    },
-    plugins: [react({ jsxRuntime: 'classic' })],
-    optimizeDeps: {
-      entries: ['app/scripts/hglib.jsx', 'test/**/*'],
-    },
-  };
-});
+  },
+  define: {
+    global: 'globalThis',
+    XYLOPHON: JSON.stringify(version),
+    'process.env.NODE_ENV': JSON.stringify(mode),
+  },
+  optimizeDeps: {
+    entries: ['app/scripts/hglib.jsx', 'test/**/*'],
+  },
+}));
