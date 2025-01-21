c151ebb8307f7b0eb809a0df04bb8ed4a72a8822
Trevor Manz <trevor.j.manz@gmail.com>
2025-01-21 18:01:53 -0500

    Migrate `HiGlassComponent/osm.test.js`

--- test/HiGlassComponent/osm.test.js
+++ test/HiGlassComponent/osm.test.js
@@ -1,10 +1,11 @@
 // @ts-nocheck
+import { afterAll, beforeAll, describe, it } from 'vitest';
 
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme from 'enzyme';
 
 import {
-  mountHGComponent,
+  mountHGComponentAsync,
   removeHGComponent,
 } from '../../app/scripts/test-helpers';
 
@@ -16,30 +17,23 @@ describe('osm', () => {
   let hgc = null;
   let div = null;
 
-  before((done) => {
-    [div, hgc] = mountHGComponent(div, hgc, osmConf, done, {
+  beforeAll(async () => {
+    [div, hgc] = await mountHGComponentAsync(div, hgc, osmConf, {
       style: 'width:800px; height:400px; background-color: lightgreen',
       bounded: false,
     });
-    // visual check that the heatmap track config menu is moved
-    // to the left
   });
 
-  after(async () => {
+  afterAll(() => {
     removeHGComponent(div);
   });
 
   it('Switches to the osm tiles track', () => {
     const { views } = hgc.instance().state;
-    // console.log('views:', views);
-
     const view = views.aa;
 
     view.tracks.center[0].type = 'osm-2d-tile-ids';
     view.tracks.center[0].uid = 'bb';
-
-    hgc.setState({
-      views,
-    });
+    hgc.setState({ views });
   });
 });
