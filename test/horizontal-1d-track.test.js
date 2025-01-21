5e15949afdf1f067a9e21b68754798b0a24cacad
Trevor Manz <trevor.j.manz@gmail.com>
2025-01-21 15:18:20 -0500

    Migrate `horizontal-1d-track.test.js`

--- test/horizontal-1d-track.test.js
+++ test/horizontal-1d-track.test.js
@@ -1,6 +1,5 @@
 // @ts-nocheck
-
-import { expect } from 'chai';
+import { afterEach, describe, expect, it } from 'vitest';
 
 // Utils
 import { waitForTilesLoaded } from '../app/scripts/test-helpers';
@@ -19,7 +18,14 @@ describe('Horizontal 1D track tests', () => {
   let viewConf;
 
   describe('Horizontal1DLineTrack with const indicator', () => {
-    it('check that the const indicators were rendered', (done) => {
+    afterEach(() => {
+      if (api?.destroy) api.destroy();
+      if (div) removeDiv(div);
+      api = undefined;
+      div = undefined;
+    });
+
+    it('check that the const indicators were rendered', async () => {
       viewConf = horizontal1dLineTrackWithConstViewConf;
 
       [div, api] = createElementAndApi(viewConf, { bound: true });
@@ -32,22 +38,24 @@ describe('Horizontal 1D track tests', () => {
         viewConf.views[0].tracks.top[0].uid,
       );
 
-      waitForTilesLoaded(hgc, () => {
-        expect(trackObj.constIndicator.children.length).to.equal(3);
-        done();
+      await new Promise((done) => {
+        waitForTilesLoaded(hgc, () => {
+          expect(trackObj.constIndicator.children.length).to.equal(3);
+          done(null);
+        });
       });
     });
+  });
 
+  describe('Horizontal1DLineTrack with const indicator', () => {
     afterEach(() => {
       if (api?.destroy) api.destroy();
       if (div) removeDiv(div);
       api = undefined;
       div = undefined;
     });
-  });
 
-  describe('Horizontal1DLineTrack with const indicator', () => {
-    it('check that the const indicators were rendered', (done) => {
+    it('check that the const indicators were rendered', async () => {
       viewConf = horizontalLineEnd;
 
       [div, api] = createElementAndApi(viewConf, { bound: true });
@@ -66,25 +74,20 @@ describe('Horizontal 1D track tests', () => {
         viewConf.views[0].tracks.top[2].uid,
       );
 
-      waitForTilesLoaded(hgc, () => {
-        // this should be
-        expect(trackObj.getDataAtPos(366)).not.to.equal(null);
+      await new Promise((done) => {
+        waitForTilesLoaded(hgc, () => {
+          // this should be
+          expect(trackObj.getDataAtPos(366)).not.to.equal(null);
 
-        // should be beyond the end of the data array because
-        // it's past the end of the last tile
-        expect(trackObj.getDataAtPos(366)).to.equal(undefined);
+          // should be beyond the end of the data array because
+          // it's past the end of the last tile
+          expect(trackObj.getDataAtPos(366)).to.equal(undefined);
 
-        // gene annotations don't currently have a mouseover function
-        expect(trackObjGeneAnnotations.getDataAtPos(10)).to.equal(null);
-        done();
+          // gene annotations don't currently have a mouseover function
+          expect(trackObjGeneAnnotations.getDataAtPos(10)).to.equal(null);
+          done(null);
+        });
       });
-    });
-
-    afterEach(() => {
-      if (api?.destroy) api.destroy();
-      if (div) removeDiv(div);
-      api = undefined;
-      div = undefined;
     });
   });
 });
