25ce4e31fe89b7989afc0d2825a7961af9ad44a1
Trevor Manz <trevor.j.manz@gmail.com>
2025-01-21 18:20:37 -0500

    Migrate `HiGlassComponent/value-interval-track.test.js`

--- test/HiGlassComponent/value-interval-track.test.js
+++ test/HiGlassComponent/value-interval-track.test.js
@@ -1,11 +1,11 @@
 // @ts-nocheck
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { expect } from 'chai';
 import Enzyme from 'enzyme';
 
 import {
-  mountHGComponent,
+  mountHGComponentAsync,
   removeHGComponent,
 } from '../../app/scripts/test-helpers';
 
@@ -17,16 +17,14 @@ describe('Value interval track', () => {
   let hgc = null;
   let div = null;
 
-  before((done) => {
-    [div, hgc] = mountHGComponent(div, hgc, twoViewConfig, done, {
+  beforeAll(async () => {
+    [div, hgc] = await mountHGComponentAsync(div, hgc, twoViewConfig, {
       style: 'width:800px; height:400px; background-color: lightgreen',
       bounded: true,
     });
-    // visual check that the heatmap track config menu is moved
-    // to the left
   });
 
-  after(async () => {
+  afterAll(() => {
     removeHGComponent(div);
   });
 
