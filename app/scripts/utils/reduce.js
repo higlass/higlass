7b1383d080fcba07e8d9509e2cad6e214bfb8ab7
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-18 09:59:31 -0500

    fix: `reduce` utility to have accumulator

--- app/scripts/utils/reduce.js
+++ app/scripts/utils/reduce.js
@@ -1,10 +1,17 @@
-// @ts-nocheck
 /**
- * Pure functional reducer. Can be used for currying stuff. Check out `flatten.js`.
- * @param   {function}  f  Reducer function.
- * @return  {array}  Curried function that accepts an array to be reduced.
+ * Pure functional reducer. Can be used for currying stuff.
+ *
+ * @see {@link ./flatten.js}
+ *
+ * @template T
+ * @template U
+ *
+ * @param {(previousValue: U, currentValue: T, currentIndex: number, array: Array<T>) => U} callbackfn
+ * @param {U} initialValue
+ * @return {(x: Array<T>) => U}  Curried function that accepts an array to be reduced.
  */
-const reduce = (f) => (x) => Array.prototype.reduce.call(x, f);
+const reduce = (callbackfn, initialValue) => (x) =>
+  x.reduce(callbackfn, initialValue);
 
 // TODO(Trevor): Not referenced anywhere. Remove?
 export default reduce;
