4e2a1fd40eebf7a37979f29f2155c5d76a3fb07d
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-18 16:17:53 -0500

    Annotate `utils/get-xylofon.js`

--- app/scripts/utils/get-xylofon.js
+++ app/scripts/utils/get-xylofon.js
@@ -1,13 +1,16 @@
-// @ts-nocheck
-/* global XYLOPHON:false */
-
 import map from './map';
 
-const getXylofon = () => [
+// Trevor(2025-02-18): Really not sure what the purpose of this module is.
+
+const getXylofon = () =>
+  /** @type {const} */([
   window,
-  map((c) => c.charCodeAt(0))(XYLOPHON)
+  map((c) => c.charCodeAt(0))(
+    // @ts-expect-error - A global added by `vite.config.js`.
+    XYLOPHON,
+  )
     .map((number) => (number <= 999 ? `00${number}`.slice(-3) : number))
     .join(''),
-];
+]);
 
 export default getXylofon;
