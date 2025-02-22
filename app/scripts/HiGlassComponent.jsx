793d43a1fb5c6e7b82ee8245c88941bf5488b512
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-22 14:03:01 -0500

    Annotated TiledPlot.js types

--- app/scripts/HiGlassComponent.jsx
+++ app/scripts/HiGlassComponent.jsx
@@ -120,6 +120,7 @@ class HiGlassComponent extends React.Component {
     this.resizeSensor = null;
 
     this.uid = slugid.nice();
+    /** @type {Record<string, import('./TiledPlot').TiledPlot> */
     this.tiledPlots = {};
     this.genomePositionSearchBoxes = {};
 
