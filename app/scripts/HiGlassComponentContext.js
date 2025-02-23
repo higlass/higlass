b7719e0b2afd86c3d2e7cc7871925db322b3f012
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-22 19:12:11 -0500

    Add explicit null

--- app/scripts/HiGlassComponentContext.js
+++ app/scripts/HiGlassComponentContext.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-const HiGlassComponentContext = React.createContext();
+const HiGlassComponentContext = React.createContext(null);
 
 export default HiGlassComponentContext;
