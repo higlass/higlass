2c596e180a3469a89c5707f4ff02780025851491
Trevor Manz <trevor.j.manz@gmail.com>
2025-03-02 14:02:05 -0500

    Remove unused testing utils

--- test/utils/empty-body.js
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-export default function emptyBody() {
-  const myNode = global.document.body;
-  while (myNode.lastElementChild) {
-    myNode.removeChild(myNode.lastElementChild);
-  }
-}
