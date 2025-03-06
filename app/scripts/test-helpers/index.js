8248688841b1794025d748dbb9fd11704a3ee825
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2025-03-05 21:10:50 -0800

    Added waitForSizeStabilization

--- app/scripts/test-helpers/index.js
+++ app/scripts/test-helpers/index.js
@@ -5,6 +5,7 @@ export {
   waitForJsonComplete,
   waitForTilesLoaded,
   waitForTransitionsFinished,
+  waitForSizeStabilization,
   mountHGComponent,
   mountHGComponentAsync,
   removeHGComponent,
