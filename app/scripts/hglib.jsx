1ee5898e375bcc8cd98b7d2dab86cb3985614728
Trevor Manz <trevor.j.manz@gmail.com>
2025-03-11 10:02:41 -0400

    Emit TypeScript types (#1261)
    * Emit TypeScript types
    
    * Update types

--- app/scripts/hglib.jsx
+++ app/scripts/hglib.jsx
@@ -20,7 +20,8 @@ export const tracks = {
   HorizontalGeneAnnotationsTrack,
 };
 
-export { default as schema } from '../schema.json';
+export { default as schema } from '../schema.json' with { type: 'json' };
+export { version } from '../../package.json' with { type: 'json' };
 
 /** @import * as api from './api' */
 /** @import * as types from './types' */
@@ -39,8 +40,6 @@ export {
 
 export { getTrackObjectFromHGC } from './utils';
 
-export { version } from '../../package.json';
-
 /**
  * Create a `HiGlassComponent` instance.
  *
@@ -50,7 +49,7 @@ export { version } from '../../package.json';
  *
  * @returns {Promise<HiGlassComponent>}
  */
-const launch = async (element, config, options) => {
+const launch = async (element, config, options = {}) => {
   return new Promise((resolve) => {
     ReactDOM.render(
       <HiGlassComponent
@@ -58,7 +57,7 @@ const launch = async (element, config, options) => {
           // Wait to resolve until React gives us a ref
           ref && resolve(ref);
         }}
-        options={options || {}}
+        options={options}
         viewConfig={config}
       />,
       element,
@@ -71,7 +70,7 @@ const launch = async (element, config, options) => {
  *
  * @param {HTMLElement} element - DOM element to render the HiGlass component.
  * @param {HiGlassViewConfig | string} viewConfig - The view config to load.
- * @param {HiGlassOptions} options - Additional options for how the HiGlass component is drawn and behaves
+ * @param {HiGlassOptions} [options] - Additional options for how the HiGlass component is drawn and behaves
  * @returns  {Promise<HiGlassApi>}  Newly created HiGlass component.
  *
  * Note: If `viewConfig` is a string, it will be interpreted as a url from which to retrieve the viewconf.
@@ -89,7 +88,7 @@ const launch = async (element, config, options) => {
  * });
  * ```
  */
-export const viewer = async (element, viewConfig, options) => {
+export const viewer = async (element, viewConfig, options = {}) => {
   const hg = await launch(
     element,
     typeof viewConfig === 'string'
