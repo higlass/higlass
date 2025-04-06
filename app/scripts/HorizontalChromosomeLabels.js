6ed4fa80ac2f638bb99bc6b5aa53654f007f0e11
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2025-04-05 23:24:09 -0700

    Remove vestigial searchfield

--- app/scripts/HorizontalChromosomeLabels.js
+++ app/scripts/HorizontalChromosomeLabels.js
@@ -5,7 +5,6 @@ import { scaleLinear } from 'd3-scale';
 
 import ChromosomeInfo from './ChromosomeInfo';
 import TiledPixiTrack from './TiledPixiTrack';
-import SearchField from './SearchField';
 
 import {
   absToChr,
@@ -28,7 +27,6 @@ class HorizontalChromosomeLabels extends TiledPixiTrack {
     const { dataConfig, animate, chromInfoPath, isShowGlobalMousePosition } =
       context;
 
-    this.searchField = null;
     this.chromInfo = null;
     this.dataConfig = dataConfig;
 
