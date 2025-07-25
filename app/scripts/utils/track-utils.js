662670198071ca1ea2b2c7524e9cc0e88e19ec81
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2025-07-25 07:11:07 -0700

    Check for options null before returning collapsed

--- app/scripts/utils/track-utils.js
+++ app/scripts/utils/track-utils.js
@@ -2,11 +2,11 @@
 const COLLAPSED_HEIGHT = 10;
 
 export const trackHeight = (track) => {
-  if (track.options.collapsed) return COLLAPSED_HEIGHT;
+  if (track.options?.collapsed) return COLLAPSED_HEIGHT;
   return track.height;
 };
 
 export const trackWidth = (track) => {
-  if (track.options.collapsed) return COLLAPSED_HEIGHT;
+  if (track.options?.collapsed) return COLLAPSED_HEIGHT;
   return track.width;
 };
